3a59661b8a184b5b4ddf14d53760d17e17cea74f
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-29 04:23:39 +0600

    sdsd

--- resources/js/sections/promo.js
+++ resources/js/sections/promo.js
@@ -13,7 +13,8 @@ function Promo(props) {
             <div className="h1 hidden-sm">Волшебное приключение<br />с Kinder Дедом Морозом</div>
             <Counter />
             <p>Оставьте заявку и подарите ребёнку волшебное видеоприключение с&nbsp;Дедушкой Морозом!!</p>
-            <Button text={`ПОДАРИТЬ ПРИКЛЮЧЕНИЕ`} onClick={toForm} />
+            <Button text={`ПОДАРИТЬ ПРИКЛЮЧЕНИЕ`} onClick={toForm} /><br/>
+            <a href="https://kinder.com/ru/ru//kinder-liven-up-the-picture" className="nhy-btn nhy-btn-blue" style={{marginTop: "20px"}}>ОЖИВЛЯЙ УПАКОВКИ</a>
         </div>
     );
 }
