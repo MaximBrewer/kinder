f346e41648e70e099b845d1da1bfc290b9118f99
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-30 00:12:30 +0600

    asa

--- resources/js/utils/scroll-parent.js
+++ resources/js/utils/scroll-parent.js
@@ -1,15 +1,12 @@
-const bdi_scrollTop = (scrolltop) => {
-    if (window.parent.postMessage) {
-        window.parent.postMessage({
-            scrolltop: scrolltop
-        }, '*');
-    }
-}
-
-export default function scrollToElement(id) {
+function scrollToElement(id) {
     let elem = document.getElementById(id)
     if (!elem) return false;
     let toY = (elem.getBoundingClientRect().top + document.scrollingElement.scrollTop) * 1;
-    bdi_scrollTop(toY);
+
+    if (window.parent.postMessage) {
+        window.parent.postMessage({
+            scrolltop: toY
+        }, '*');
+    }
     return false;
 };
