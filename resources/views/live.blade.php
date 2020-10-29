2490c35d4dafad89f2018c1351b59edac30a5133
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-29 16:56:31 +0600

    sdsd

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -11,7 +11,7 @@
 <body style="margin:0;padding:0;">
     <iframe src="/live-iframe" style="height:100vh;width:100%;margin:0;padding:0;" frameborder="0" id="iframe"></iframe>
     <script type="text/javascript">
-        document.getElementById("iframe").style.height = document.getElementById("iframe").offsetHeight + "px";
+        document.getElementById("iframe").style.height = window.innerHeight + "px";
         function bdi_resizeIframe() {
             if (parent.postMessage) {
                 var body = document.getElementsByTagName("BODY")[0];
