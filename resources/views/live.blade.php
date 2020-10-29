dfe12954f7506223e0ea08a47c394709e57f24bc
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-29 16:32:04 +0600

    sdsd

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -6,201 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kinder New Year</title>
-    <link rel="preload" href="/assets/img/promo-back.png" as="image" media="(min-width: 1280px)">
-    <link rel="preload" href="/assets/img/moroz_sneg-2.png" as="image" media="(min-width: 1280px)">
-    <link rel="preload" href="/assets/img/promo-presents.png" as="image" media="(min-width: 1280px)">
-    <link rel="preload" href="/assets/img/promo-bublle.png" as="image" media="(min-width: 1280px)">
-    <link rel="stylesheet" href="/assets/css/style.css">
-
 </head>
 
 <body>
-    <div id="trigger" class="steps__trigger"></div>
-    <h1 class="visually-hidden">Kinder New Year</h1>
-    <section class="promo">
-        <div class="container promo__container">
-            <div class="promo__shine promo__shine--1"></div>
-            <div class="promo__shine promo__shine--2"></div>
-            <div class="promo__shine promo__shine--3"></div>
-            <div class="promo__shine promo__shine--4"></div>
-            <div class="promo__shine promo__shine--5"></div>
-            <div class="promo__shine promo__shine--6"></div>
-            <div class="promo__shine promo__shine--7"></div>
-            <div class="promo__shine promo__shine--8"></div>
-            <div class="promo__shine promo__shine--9"></div>
-            <div class="promo__shine promo__shine--10"></div>
-            <img class="promo__snow promo__snow--1" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--2" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--3" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--4" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--5" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--6" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--7" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--8" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--9" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--10" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--11" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--12" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--13" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--14" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--15" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--16" src="/assets/img/promo-snowflake.png" width="110" height="100" alt="">
-            <img class="promo__snow promo__snow--17" src="/assets/img/promo-snowflake-all.png" width="186" height="176" alt="">
-            <div class="promo__presents">
-                <img class="promo__presents-photo" src="/assets/img/promo-presents.png" alt="Подарки от Снегурочки и Деда Мороза">
-            </div>
-            <div class="promo__guests">
-                <picture>
-                    <source media="(max-width: 767px)" srcset="/assets/img/promo-guests-mobile.png">
-                    <img src="/assets/img/moroz_sneg-2.png" alt="Снегурочка и дед мороз">
-                </picture>
-            </div>
-            <div class="promo__bublle">
-                <img class="promo__blue-snowflake promo__blue-snowflake--1" src="/assets/img/blue-snowflake-3.png" width="24" height="20" alt="снежинка">
-                <img class="promo__blue-snowflake promo__blue-snowflake--2" src="/assets/img/blue-snowflake-2.png" width="93" height="88" alt="снежинка">
-                <img class="promo__blue-snowflake promo__blue-snowflake--3" src="/assets/img/blue-snowflake-3.png" width="24" height="20" alt="снежинка">
-                <img class="promo__blue-snowflake promo__blue-snowflake--4" src="/assets/img/blue-snowflake-4.png" width="24" height="20" alt="снежинка">
-                <img class="promo__blue-snowflake promo__blue-snowflake--5" src="/assets/img/blue-snowflake-5.png" width="24" height="20" alt="снежинка">
-                <img class="promo__snowball promo__snowball--1" src="/assets/img/promo-snowball.png" width="8" height="7" alt="снег">
-                <img class="promo__snowball promo__snowball--2" src="/assets/img/promo-snowball.png" width="8" height="7" alt="снег">
-                <img class="promo__snowball promo__snowball--3" src="/assets/img/promo-snowball.png" width="8" height="7" alt="снег">
-                <img class="promo__snowball promo__snowball--4" src="/assets/img/promo-snowball.png" width="8" height="7" alt="снег">
-                <img class="promo__snowball promo__snowball--5" src="/assets/img/promo-snowball.png" width="8" height="7" alt="снег">
-                <p class="promo__bublle-text">Новогоднее волшебство<br>на&nbsp;каждой* упаковке!
-                </p>
-            </div>
-        </div>
-    </section>
-
-
-    <div class="wrapper-steps">
-        <section class="steps steps--1">
-            <div class="steps__snow-wrapper">
-                <div class="steps__snow steps__snow--top"></div>
-                <div class="steps__snow steps__snow--bottom"></div>
-                <div class="steps__snow-interactive"></div>
-            </div>
-            <div class="steps__snowdrift"></div>
-            <div class="steps__overflow-wrapper">
-                <div class="container steps__container">
-                    <ul class="steps__list">
-                        <li class="steps__item steps__item--1">
-                            <h3 class="steps__item-count">Шаг 1</h3>
-                            <p class="steps__item-action">Сканируйте<br class="steps__item-br steps__item-br--tablet"> QR&#8209;код на&nbsp;новогодних упаковках<sup>*</sup> Kinder</p>
-                            <div class="steps__item-interactive steps__item-interactive--first">
-                                <!-- <img class="steps__item-interactive-img" src="/assets/img/interactive/ipad/1-min.png" alt=""> -->
-                                <div class="steps__item-interactive-img steps__item-interactive-img--1">
-                                    <img src="/assets/img/qr-mobile.gif" type="image/gif" alt="">
-                                </div>
-                                <img class="steps__item-interactive-border" src="/assets/img/interactive/ipad/phone-min.png" alt="">
-                            </div>
-                        </li>
-                        <li class="steps__item steps__item--2">
-                            <h3 class="steps__item-count">Шаг 2</h3>
-                            <p class="steps__item-action">Оживите героев на&nbsp;упаковке</p>
-                            <div class="steps__item-interactive">
-                                <img class="steps__item-interactive-img" src="/assets/img/interactive/ipad/2-min.png" alt="">
-                                <img class="steps__item-interactive-border" src="/assets/img/interactive/ipad/phone-min.png" alt="">
-                            </div>
-                        </li>
-                        <li class="steps__item steps__item--3">
-                            <h3 class="steps__item-count">Шаг 3</h3>
-                            <p class="steps__item-action">Окунитесь <br>в&nbsp;сказочный мир игр</p>
-                            <div class="steps__item-interactive">
-                                <img class="steps__item-interactive-img" src="/assets/img/interactive/ipad/3-min.png" alt="">
-                                <img class="steps__item-interactive-border" src="/assets/img/interactive/ipad/phone-min.png" alt="">
-                            </div>
-                        </li>
-                        <li class="steps__item steps__item--4">
-                            <h3 class="steps__item-count">Шаг 4</h3>
-                            <p class="steps__item-action">Станьте героем&nbsp;сказки!<br>Создайте волшебные новогодние образы!</p>
-                            <div class="steps__item-interactive">
-                                <img class="steps__item-interactive-img" src="/assets/img/interactive/ipad/4-min.png" alt="">
-                                <img class="steps__item-interactive-border" src="/assets/img/interactive/ipad/phone-min.png" alt="">
-                            </div>
-                        </li>
-                    </ul>
-
-                    <div class="steps__interactive">
-                        <div class="steps__interactive-choko">
-                            <img src="/assets/img/steps-choko.png" alt="Шоколад киндер">
-                        </div>
-                        <div class="steps__interactive-phone">
-                            <!-- <img class="steps__interactive-content steps__interactive-content--1" src="/assets/img/interactive/desc/1-min.png" alt="Анимации сканированияф кода"> -->
-                            <div class="steps__interactive-content steps__interactive-content--1">
-                                <!-- <img src="/assets/img/qr.gif" alt="Анимации сканированияф кода" type="image/gif"> -->
-                                <div class="spritesheets spritesheets--qr"></div>
-                            </div>
-                            <img class="steps__interactive-content steps__interactive-content--2" src="/assets/img/interactive/desc/2-min.png" alt="Анимации оживления героев">
-                            <img class="steps__interactive-content steps__interactive-content--3" src="/assets/img/interactive/desc/3-min.png" alt="Анимации сказочного мира">
-                            <img class="steps__interactive-content steps__interactive-content--4" src="/assets/img/interactive/desc/4-min.png" alt="Анимации создания образа">
-                            <div class="steps__interactive-border"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="steps__wave"></div>
-        </section>
-    </div>
-
-    <section class="video">
-        <div class="video__wave"></div>
-        <div class="container video__container">
-            <img class="video__star video__star--1" src="/assets/img/video-star.png" width="43" height="44" alt="звездочка">
-            <img class="video__star video__star--2" src="/assets/img/video-star.png" width="43" height="44" alt="звездочка">
-            <img class="video__snowflake video__snowflake--1" src="/assets/img/video-snowflake.png" width="54" height="50" alt="снежинка">
-            <img class="video__snowflake video__snowflake--2" src="/assets/img/video-snowflake-2.png" width="54" height="50" alt="снежинка">
-            <img class="video__snowflake video__snowflake--3" src="/assets/img/video-snowflake-3.png" width="54" height="50" alt="снежинка">
-            <div class="video__wrapper-img">
-                <picture>
-                    <source media="(max-width: 767px)" srcset="/assets/img/video-mobile.png">
-                    <img src="/assets/img/video-content2.png" alt="Специальная упаковка с видео">
-                </picture>
-                <img class="video__spray" src="/assets/img/video-spray.png" alt="" width="186" height="209">
-            </div>
-            <div class="video__text-content">
-                <h2 class="video__title">Любимые герои &laquo;Маша и&nbsp;Медведь!&raquo;</h2>
-                <p class="video__text">Cмотри уникальное новогоднее видео на&nbsp;специальных упаковках**</p>
-            </div>
-        </div>
-    </section>
-
-    <section class="qr">
-        <div class="qr__wave"></div>
-        <div class="container">
-            <h2 class="qr__title">Сканируйте QR&#8209;код&nbsp;с помощью:</h2>
-            <ul class="qr__list">
-                <li class="qr__list-item">
-                    <img class="qr__list-icon qr__list-icon--cam" src="/assets/img/tel-icon-cam.svg" alt="Камера" width="49" height="40">
-                    <p class="qr__list-text">
-                        Камеры телефона
-                    </p>
-                </li>
-                <li class="qr__list-item">
-                    <img class="qr__list-icon qr__list-icon--qr" src="/assets/img/tel-icon-qr.svg" alt="QR-код" width="50" height="51">
-                    <p class="qr__list-text">
-                        QR-ридера
-                    </p>
-                </li>
-                <li class="qr__list-item">
-                    <img class="qr__list-icon qr__list-icon--vk" src="/assets/img/tel-icon-vk.svg" alt="Вконтакте" width="47" height="46">
-                    <p class="qr__list-text">
-                        Приложения VK
-                    </p>
-                </li>
-                <li class="qr__list-item">
-                    <img class="qr__list-icon qr__list-icon--chrome" src="/assets/img/tel-icon-chrome.svg" alt="Браузер" width="47" height="48">
-                    <p class="qr__list-text qr__list-text--chrome">
-                        Браузера Chrome
-                    </p>
-                </li>
-            </ul>
-            <p class="qr__annotation">
-                * В&nbsp;активации участвуют все упаковки Kinder с&nbsp;новогодним дизайном, кроме Kinder Сюрприз
-                20г/36г/60г/100г/220г,<br class="qr__annotation-br"> Kinder Joy 20г, Kinder Фигурный Шоколад 55г/110г, Kinder Schoko-bons 46г/125г. <br>
-                ** В&nbsp;активации участвуют наборы кондитерских изделий Kinder Mini Mix 106,5г; Kinder Mix 199г и Kinder Maxi Mix 223г. </p>
-        </div>
-    </section>
+    <iframe src="/live" style="width:100vw;height:100vh;"></iframe>
     <script type="text/javascript">
         function bdi_resizeIframe() {
             if (parent.postMessage) {
@@ -227,10 +36,10 @@
         });
         // window.addEventListener('load', bdi_resizeIframe);
         var bdiTO;
-        // window.addEventListener('resize', function() {
-        //     clearTimeout(bdiTO);
-        //     bdiTO = setTimeout(bdi_resizeIframe, 500);
-        // });
+        window.addEventListener('resize', function() {
+            clearTimeout(bdiTO);
+            bdiTO = setTimeout(bdi_resizeIframe, 500);
+        });
 
         // Back to top
         document.getElementsByClassName('.back_to_top').length &&
@@ -238,8 +47,6 @@
                 bdi_scrollTop(0)
             });
     </script>
-    <script src="/assets/js/vendors.js"></script>
-    <script src="/assets/js/bundle.js"></script>
 </body>
 
 </html>
