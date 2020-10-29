9bd3e0d4102095d3845a45537e3bb4726ba42317
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-29 17:01:05 +0600

    asas

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -11,7 +11,7 @@
 <body style="margin:0;padding:0;">
     <iframe src="/live-iframe" style="height:100vh;width:100%;margin:0;padding:0;" frameborder="0" id="iframe"></iframe>
     <script type="text/javascript">
-        console.log(parent.innerHeight)
+        console.log(parent)
         document.getElementById("iframe").style.height = window.innerHeight + "px";
 
         function bdi_resizeIframe() {
