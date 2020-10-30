b285edba7cc71b473aa74a367088dfaa54f8a328
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-31 02:21:22 +0600

    sdsd

--- public/js/app.js
+++ public/js/app.js
@@ -68252,28 +68252,7 @@ function Gifts(props) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "styles", function() { return styles; });
 var styles = {
-  content: {
-    borderWidth: "0",
-    backgroundColor: "#07A1F3",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundImage: "url(/img/popup-bg.png)",
-    backgroundSize: "cover",
-    boxShadow: "0px 0px 79px #000000",
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    fontFamily: '"bubbleboddy_neue", sans-serif',
-    padding: "60px",
-    marginRight: '-50%',
-    transform: 'translateX(-50%)',
-    maxWidth: "660px",
-    borderRadius: "40px",
-    fontSize: "28px",
-    textAlign: "center",
-    color: "#ffffff"
-  },
+  content: {},
   overlay: {
     zIndex: 100
   }
--- resources/js/styles/modal.js
+++ resources/js/styles/modal.js
@@ -1,25 +1,5 @@
 export const styles = {
     content: {
-        borderWidth: "0",
-        backgroundColor: "#07A1F3",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundImage: "url(/img/popup-bg.png)",
-        backgroundSize: "cover",
-        boxShadow: "0px 0px 79px #000000",
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        fontFamily: '"bubbleboddy_neue", sans-serif',
-        padding: "60px",
-        marginRight: '-50%',
-        transform: 'translateX(-50%)',
-        maxWidth: "660px",
-        borderRadius: "40px",
-        fontSize: "28px",
-        textAlign: "center",
-        color: "#ffffff"
     },
     overlay: {
         zIndex: 100
