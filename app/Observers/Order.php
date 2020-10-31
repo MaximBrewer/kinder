b3d5d365297775183cc93c103d8688d61eedd475
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-31 20:36:01 +0600

    sdsd

--- app/Observers/Order.php
+++ app/Observers/Order.php
@@ -22,7 +22,7 @@ class Order
                 case "confirmed":
                     try {
                         $unsubscribe = "https://kinder.gpucloud.ru/unsubscribe?email=" . $order->email . "&email_hash=" . $order->email_hash;
-                        Mail::to($order->email)->send(new \App\Mail\Frame1($unsubscribe));
+                        Mail::to($order->email)->send(new \App\Mail\Frame2($unsubscribe));
                         event(new Refresh());
                     } catch (Throwable $e) {
                         report($e);
