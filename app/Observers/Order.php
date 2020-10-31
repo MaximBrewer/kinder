56bbb5bf5d937d3a76cbadb66d9915faaad733b5
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-31 18:50:22 +0600

    sdsd

--- app/Observers/Order.php
+++ app/Observers/Order.php
@@ -13,16 +13,15 @@ class Order
     public function updated(ModelsOrder $order)
     {
         if ($order->wasChanged('status')) {
-
             switch ($order->status) {
                 case "new":
                     break;
                 case "canceled":
                     break;
                 case "confirmed":
+                    $unsubscribe = "https://kinder.gpucloud.ru/unsubscribe?email=" . $order->email . "&email_hash=" . $order->email_hash;
+                    Mail::to($order->email)->send(new \App\Mail\Frame1($unsubscribe));
                     try {
-                        $unsubscribe = "https://kinder.gpucloud.ru/unsubscribe?email=" . $order->email . "&email_hash=" . $order->email_hash;
-                        Mail::to($order->email)->send(new \App\Mail\Frame1($unsubscribe));
                         event(new Refresh());
                     } catch (Throwable $e) {
                         report($e);
