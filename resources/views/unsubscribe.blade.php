599a0821e28e42ba6d626c2ed2179660865fc681
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-31 21:41:11 +0600

    DF

--- resources/views/unsubscribe.blade.php
+++ resources/views/unsubscribe.blade.php
@@ -1,10 +1,22 @@
-<div class="form-wrapper">
-    <div>
-        <div class="nhy-container">
-            <div id="formEl" class="form-hny"><a></a>
-                <div class="h1">ЗАПОЛНИТЕ ЗАЯВКУ</div>
-                <p>Расскажите о главных достижениях ребёнка в этом году, его хобби и выберите подарок, который подарит Дед Мороз</p>
+<!DOCTYPE html>
+<html lang="ru-RU" dir="ltr">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/css/app.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="form-wrapper">
+        <div>
+            <div class="nhy-container">
+                <div id="formEl" class="form-hny"><a></a>
+                    <div class="h1">ЗАПОЛНИТЕ ЗАЯВКУ</div>
+                    <p>Расскажите о главных достижениях ребёнка в этом году, его хобби и выберите подарок, который подарит Дед Мороз</p>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</body>
+
+</html>
