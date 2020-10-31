3d3dd9b4e3cc42e57591d6db9ad5d9c0d1d194d3
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-31 16:32:01 +0600

    sd

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    /**
+     * Scope a query to only include popular users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeNew($query)
+    {
+        return $query->where('status', 'new');
+    }
 }
