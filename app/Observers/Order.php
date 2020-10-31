95cc4ee9b9db45ced26d19c50a8f707b89b4b83b
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-31 19:57:59 +0600

    ывыв

--- app/Observers/Order.php
+++ app/Observers/Order.php
@@ -18,6 +18,13 @@ class Order
                 case "new":
                     break;
                 case "canceled":
+                    try {
+                        $unsubscribe = "https://kinder.gpucloud.ru/unsubscribe?email=" . $order->email . "&email_hash=" . $order->email_hash;
+                        Mail::to($order->email)->send(new \App\Mail\Frame4($unsubscribe));
+                        event(new Refresh());
+                    } catch (Throwable $e) {
+                        report($e);
+                    }
                     break;
                 case "confirmed":
                     try {
