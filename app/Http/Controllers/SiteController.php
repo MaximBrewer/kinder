dba687c55826b261a5080dc2306691e831ce5720
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-31 18:15:21 +0600

    dfd

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -64,7 +64,7 @@ class SiteController extends Controller
             'gift_id' => $request->gift,
             'hash' => $request->gender,
             'email_hash' => $request->gender,
-            'news' => intVal($request->news),
+            'news' => (int)$request->news,
             'status' => 'new',
         ]);
 
