05306959c6b79d0c8d38e778819411ec7da8abf1
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-10-31 16:01:15 +0600

    sdds

--- resources/views/mail/frame2.blade.php
+++ resources/views/mail/frame2.blade.php
@@ -1,110 +1,79 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title></title>
-        <style>
-            a.btn:active {
-                top: 1px;
-            }
-        </style>
-    </head>
 
-    <body style="margin:0;padding:0;font-family: Noteworthy;">
-        <div
-            style="width:800px;height:1200px;background-image:url(https://kinder.gpucloud.ru/images/mail-bg.jpg);background-size:cover;"
-        >
-            <div style="padding: 249px 117px 0px 172px;">
-                <div style="min-height: 780px;">
-                    <table style="margin:0 0 0 auto;">
-                        <tr style="vertical-align: baseline;">
-                            <td>
-                                <img
-                                    src="https://kinder.gpucloud.ru/img/promo-logo.png"
-                                    alt=""
-                                    style="position:relative;top:6px;height: 60px;width:203px;margin-right: 4px;"
-                                />
-                            </td>
-                        </tr>
-                    </table>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <style>
+        a.btn:active {
+            top: 1px;
+        }
+    </style>
+</head>
 
-                    <h1
-                        style="margin: 25px 0 35px;font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;text-shadow: 1px 1px 0px rgba(0, 0, 0), -1px 1px 0px rgba(0, 0, 0), 1px -1px 0px rgba(0, 0, 0), -1px -1px 0px rgba(0, 0, 0), 0px 4px 4px rgba(0, 0, 0, 0.25);text-shadow: none;"
-                    >
-                        Дедушка Мороз <br />
-                        снова ждет Вашу заявку на <br />
-                        поздравления!
-                    </h1>
-                    <div
-                        style="font-size: 17px;line-height: 24px;text-align: center;color: #000000;"
-                    >
-                        <p style="margin-bottom:24px;">
-                            В прошлом году Вы принимали участие в акции <br />
-                            «Kinder Дед Мороз приходит в гости» <br />
-                            и получили видеопоздравление!
-                        </p>
-                        <p>
-                            Заполните заявку на сайте
-                            <a
-                                href="https://www.kinder.com/ru/ru/kinder-new-year"
-                                style="color: #F04E23 !important;"
-                                >www.kinder.com</a
-                            >
-                            в этом году <br />
-                            и подарите ребёнку новое незабываемое
-                            видеопоздравление
-                            <br />
-                            от Kinder Дедушки Мороза!
-                        </p>
-                    </div>
-                    <h2
-                        style="font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;margin-bottom: 0;"
-                    >
-                        Новый год приходит <br />
-                    </h2>
-                    <table style="margin:0 auto;">
-                        <tr style="vertical-align: baseline;">
-                            <td>
-                                <h2
-                                    style="font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;"
-                                >
-                                    с
-                                </h2>
-                            </td>
-                            <td>
-                                <img
-                                    src="https://kinder.gpucloud.ru/img/promo-logo.png"
-                                    alt=""
-                                    style="position:relative;top:6px;height: 60px;width:203px;margin-right: 4px;"
-                                />
-                            </td>
-                            <td>
-                                <h2
-                                    style="font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;"
-                                >
-                                    !
-                                </h2>
-                            </td>
-                        </tr>
-                    </table>
+<body style="margin:0;padding:0;font-family: Noteworthy;">
+    <div style="width:800px;height:1200px;background-image:url(https://kinder.gpucloud.ru/images/mail-bg.jpg);background-size:cover;">
+        <div style="padding: 249px 117px 0px 172px;">
+            <div style="min-height: 780px;">
+                <table style="margin:0 0 0 auto;">
+                    <tr style="vertical-align: baseline;">
+                        <td>
+                            <img src="https://kinder.gpucloud.ru/img/promo-logo.png" alt="" style="position:relative;top:6px;height: 60px;width:203px;margin-right: 4px;" />
+                        </td>
+                    </tr>
+                </table>
 
-                    <div style="text-align: center;">
-                        <a
-                            class="btn"
-                            href="https://www.kinder.com/ru/ru/kinder-new-year"
-                            style="text-decoration:none;font-weight:700;outline:none !important;box-shadow:none;padding:18px 35px;font-family: Noteworthy;font-size: 15px;line-height: 23px;border:none;background: #E54C2E;border-radius: 100px;display: inline-block;position: relative;color:white !important"
-                        >
-                            ОТПРАВИТЬ ЗАЯВКУ
-                        </a>
-                    </div>
+                <h1 style="margin: 25px 0;font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;text-shadow: 1px 1px 0px rgba(0, 0, 0), -1px 1px 0px rgba(0, 0, 0), 1px -1px 0px rgba(0, 0, 0), -1px -1px 0px rgba(0, 0, 0), 0px 4px 4px rgba(0, 0, 0, 0.25);text-shadow: none;">
+                    Дедушка Мороз <br />
+                    получил Вашу заявку!
+                </h1>
+                <div style="font-size: 17px;line-height: 24px;text-align: center;color: #000000;">
+                    <p style="margin-bottom:14px;">
+                        Дедушка Мороз и Снегурочка уже начали готовить<br />
+                        видеопоздравление! В скором времени Ваш ребёнок
+                        сможет <br />
+                        побывать у них в гостях!
+                    </p>
+                    <p>
+                        Пока мы готовим для Вас новогоднее чудо,<br />
+                        не забудьте купить подарок Kinder, <br />
+                        ведь без него сказка не будет такой волшебной!
+                    </p>
                 </div>
-            </div>
-            <div style="text-align: center;">
-                <a
-                    href="{{ $unsubscribe }}"
-                    style="color: #40160B !important;font-size: 20px;"
-                    >Отписаться от рассылки</a
-                >
+                <h2 style="font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;margin-bottom: 20px;">
+                    КУПИТЬ ПОДАРОК:
+                </h2>
+                <table style="margin:0 auto;">
+                    <tr style="vertical-align: baseline;">
+                        <td>
+                            <a href="https://pokupki.market.yandex.ru/search?cvredirect=2&utm_source=Kinder&utm_medium=website&utm_campaign=KinderNewYear2020&text=%D0%BA%D0%B8%D0%BD%D0%B4%D0%B5%D1%80&glfilter=7893318%3A10715858" style="display: block;margin:0 15px 10px auto;width:171px;padding:4px 0;border-radius:20px;background: #fff;overflow:hidden;">
+                                <img src="https://kinder.gpucloud.ru/images/yandex-mail.png" style="display:block;width:114px;height:32px;margin:0 auto;" />
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://www.ozon.ru/brand/kinder-138860371/?utm_source=Kinder&utm_medium=website&utm_campaign=KinderNewYear2020" style="display: block;margin:0 auto 10px 15px;width:171px;padding:10px 0;border-radius:20px;background: #fff;overflow:hidden;">
+                                <img src="https://kinder.gpucloud.ru/images/ozon-mail.png" style="display:block;width:95px;height:20px;margin:0 auto;" />
+                            </a>
+                        </td>
+                    </tr>
+                    <tr style="vertical-align: baseline;">
+                        <td>
+                            <a href="https://www.vprok.ru/catalog/1450/shokolad-batonchiki/brend/kinder?utm_source=Kinder&utm_medium=website&utm_campaign=KinderNewYear2020" style="display: block;margin:0 15px 10px auto;width:171px;padding:4px 0;border-radius:20px;background: #fff;overflow:hidden;">
+                                <img src="https://kinder.gpucloud.ru/images/cross-mail.png" style="display:block;width:120px;height:32px;margin:0 auto;" />
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://www.utkonos.ru/search/kinder/cat/52?&utm_source=Kinder&utm_medium=website&utm_campaign=KinderNewYear2020" style="display: block;margin:0 auto 10px 15px;width:171px;padding:5px 0;border-radius:20px;background: #fff;overflow:hidden;">
+                                <img src="https://kinder.gpucloud.ru/images/utkonos-mail.png" style="display:block;width:114px;height:31px;margin:0 auto;" />
+                            </a>
+                        </td>
+                    </tr>
+                </table>
             </div>
         </div>
-    </body>
+        <div style="text-align: center;">
+            <a href="{{ $unsubscribe }}" style="color: #40160B !important;font-size: 20px;">Отписаться от рассылки</a>
+        </div>
+    </div>
+</body>
+
 </html>
--- resources/views/mail/frame4.blade.php
+++ resources/views/mail/frame4.blade.php
@@ -1,109 +1,58 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title></title>
-        <style>
-            a.btn:active {
-                top: 1px;
-            }
-        </style>
-    </head>
 
-    <body style="margin:0;padding:0;font-family: Noteworthy;">
-        <div
-            style="width:800px;height:1200px;background-image:url(https://kinder.gpucloud.ru/images/mail-bg.jpg);background-size:cover;"
-        >
-            <div style="padding: 249px 117px 0px 172px;">
-                <div style="min-height: 780px;">
-                    <table style="margin:0 0 0 auto;">
-                        <tr style="vertical-align: baseline;">
-                            <td>
-                                <img
-                                    src="https://kinder.gpucloud.ru/img/promo-logo.png"
-                                    alt=""
-                                    style="position:relative;top:6px;height: 60px;width:203px;margin-right: 4px;"
-                                />
-                            </td>
-                        </tr>
-                    </table>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <style>
+        a.btn:active {
+            top: 1px;
+        }
+    </style>
+</head>
 
-                    <h1
-                        style="margin: 25px 0 35px;font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;text-shadow: 1px 1px 0px rgba(0, 0, 0), -1px 1px 0px rgba(0, 0, 0), 1px -1px 0px rgba(0, 0, 0), -1px -1px 0px rgba(0, 0, 0), 0px 4px 4px rgba(0, 0, 0, 0.25);text-shadow: none;"
-                    >
-                    Дедушка Мороз <br />
-получил Вашу заявку!
-                    </h1>
-                    <div
-                        style="font-size: 17px;line-height: 24px;text-align: center;color: #000000;"
-                    >
-                        <p style="margin-bottom:24px;">
-                            В прошлом году Вы принимали участие в акции <br />
-                            «Kinder Дед Мороз приходит в гости» <br />
-                            и получили видеопоздравление!
-                        </p>
-                        <p>
-                            Заполните заявку на сайте
-                            <a
-                                href="https://www.kinder.com/ru/ru/kinder-new-year"
-                                style="color: #F04E23 !important;"
-                                >www.kinder.com</a
-                            >
-                            в этом году <br />
-                            и подарите ребёнку новое незабываемое
-                            видеопоздравление
-                            <br />
-                            от Kinder Дедушки Мороза!
-                        </p>
-                    </div>
-                    <h2
-                        style="font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;margin-bottom: 0;"
-                    >
-                        Новый год приходит <br />
-                    </h2>
-                    <table style="margin:0 auto;">
-                        <tr style="vertical-align: baseline;">
-                            <td>
-                                <h2
-                                    style="font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;"
-                                >
-                                    с
-                                </h2>
-                            </td>
-                            <td>
-                                <img
-                                    src="https://kinder.gpucloud.ru/img/promo-logo.png"
-                                    alt=""
-                                    style="position:relative;top:6px;height: 60px;width:203px;margin-right: 4px;"
-                                />
-                            </td>
-                            <td>
-                                <h2
-                                    style="font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;"
-                                >
-                                    !
-                                </h2>
-                            </td>
-                        </tr>
-                    </table>
+<body style="margin:0;padding:0;font-family: Noteworthy;">
+    <div style="width:800px;height:1200px;background-image:url(https://kinder.gpucloud.ru/images/mail-bg.jpg);background-size:cover;">
+        <div style="padding: 249px 117px 0px 172px;">
+            <div style="min-height: 780px;">
+                <table style="margin:0 0 0 auto;">
+                    <tr style="vertical-align: baseline;">
+                        <td>
+                            <img src="https://kinder.gpucloud.ru/img/promo-logo.png" alt="" style="position:relative;top:6px;height: 60px;width:203px;margin-right: 4px;" />
+                        </td>
+                    </tr>
+                </table>
 
-                    <div style="text-align: center;">
-                        <a
-                            class="btn"
-                            href="https://www.kinder.com/ru/ru/kinder-new-year"
-                            style="text-decoration:none;font-weight:700;outline:none !important;box-shadow:none;padding:18px 35px;font-family: Noteworthy;font-size: 15px;line-height: 23px;border:none;background: #E54C2E;border-radius: 100px;display: inline-block;position: relative;color:white !important"
-                        >
+                <h1 style="margin: 25px 0;font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;text-shadow: 1px 1px 0px rgba(0, 0, 0), -1px 1px 0px rgba(0, 0, 0), 1px -1px 0px rgba(0, 0, 0), -1px -1px 0px rgba(0, 0, 0), 0px 4px 4px rgba(0, 0, 0, 0.25);text-shadow: none;">
+                    Ваша заявка не прошла<br />
+                    модерацию
+                </h1>
+                <div style="font-size: 17px;line-height: 24px;text-align: center;color: #000000;">
+                    <p style="margin-bottom: 30px;">
+                        К сожалению, Ваша заявка не прошла модерацию.
+                    </p>
+                    <p>
+                        Ознакомьтесь, пожалуйста, с правилами акции.
+                    </p>
+                    <p style="margin-bottom: 30px;">
+                        <a class="btn" href="https://kinder.gpucloud.ru/rules.pdf" style="width:180px;text-decoration:none;font-weight:700;outline:none !important;box-shadow:none;padding:18px 35px;font-family: Noteworthy;font-size: 15px;line-height: 23px;border:none;background: #E54C2E;border-radius: 100px;display: inline-block;position: relative;color:white !important">
+                            ПРАВИЛА АКЦИИ
+                        </a>
+                    </p>
+                    <p>
+                        Попробуйте отправить заявку снова!
+                    </p>
+                    <p>
+                        <a class="btn" href="https://www.kinder.com/ru/ru/kinder-new-year" style="width:180px;text-decoration:none;font-weight:700;outline:none !important;box-shadow:none;padding:18px 35px;font-family: Noteworthy;font-size: 15px;line-height: 23px;border:none;background: #E54C2E;border-radius: 100px;display: inline-block;position: relative;color:white !important">
                             ОТПРАВИТЬ ЗАЯВКУ
                         </a>
-                    </div>
+                    </p>
                 </div>
             </div>
-            <div style="text-align: center;">
-                <a
-                    href="{{ $unsubscribe }}"
-                    style="color: #40160B !important;font-size: 20px;"
-                    >Отписаться от рассылки</a
-                >
-            </div>
         </div>
-    </body>
+        <div style="text-align: center;">
+            <a href="{{ $unsubscribe }}" style="color: #40160B !important;font-size: 20px;">Отписаться от рассылки</a>
+        </div>
+    </div>
+</body>
+
 </html>
