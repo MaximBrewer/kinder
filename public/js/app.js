70a30d28d398cc999a06c3a5d4a7d9e2fe1194f5
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-02 14:27:33 +0600

    sd

--- public/js/app.js
+++ public/js/app.js
@@ -67104,6 +67104,33 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+
+if (typeof Object.assign != "function") {
+  Object.assign = function (target) {
+    "use strict";
+
+    if (target == null) {
+      throw new TypeError("Cannot convert undefined or null to object");
+    }
+
+    target = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+
+    return target;
+  };
+}
+
 window.axios = axios__WEBPACK_IMPORTED_MODULE_2___default.a; // Enable pusher logging - don't include this in production
 // Pusher.logToConsole = true;
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,29 @@ import Counter from "./utils/counter";
 import Form from "./sections/form";
 import Gifts from "./sections/gifts";
 import Faq from "./sections/faq";
+
+if (typeof Object.assign != "function") {
+    Object.assign = function(target) {
+        "use strict";
+        if (target == null) {
+            throw new TypeError("Cannot convert undefined or null to object");
+        }
+
+        target = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+                for (var key in source) {
+                    if (Object.prototype.hasOwnProperty.call(source, key)) {
+                        target[key] = source[key];
+                    }
+                }
+            }
+        }
+        return target;
+    };
+}
+
 window.axios = axios;
 
 // Enable pusher logging - don't include this in production
