b596b6931cff3d8b2d501489f229152b327a5b74
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-05 19:00:06 +0600

    sds

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -97,7 +97,7 @@ class SiteController extends Controller
      */
     public function video(Request $request, $hash)
     {
-        return view('playlist', ['hash' => $hash]);
+        return view('video', ['hash' => $hash]);
     }
 
     /**
