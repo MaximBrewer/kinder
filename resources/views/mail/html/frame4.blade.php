00e19ddc7008deb562a2e4381246c7eee82a5262
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-07 17:32:24 +0600

    sd

--- resources/views/mail/html/frame4.blade.php
+++ resources/views/mail/html/frame4.blade.php
@@ -21,51 +21,91 @@
         * {
             font-weight: bold;
         }
+
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Noteworthy;
+            font-weight: bold;
+        }
+
+        .body-inner {
+            margin: 0;
+            padding: 0;
+            transform: scale(.4);
+        }
+
+
+        @media (min-width: 375px) {
+            .body-inner {
+                transform: scale(.46875);
+            }
+        }
+
+        @media (min-width: 414px) {
+            .body-inner {
+                transform: scale(.5175);
+            }
+        }
+
+        @media (min-width: 768px) {
+            .body-inner {
+                transform: scale(.96);
+            }
+        }
+
+        @media (min-width: 800px) {
+            .body-inner {
+                transform: none;
+            }
+        }
     </style>
 </head>
 
-<body style="margin:0;padding:0;font-family: Noteworthy;font-weight:bold;">
-    <div style="width:800px;height:5px;background:#000;"></div>
-    <div style="width:800px;height:1200px;background-image:url(https://kinder.gpucloud.ru/images/mail-bg.jpg);background-size:cover;">
-        <div style="padding: 249px 117px 0px 172px;">
-            <div style="min-height: 780px;">
-                <table style="margin:0 0 0 auto;">
-                    <tr style="vertical-align: baseline;">
-                        <td>
-                            <img src="https://kinder.gpucloud.ru/img/promo-logo.png" alt="" style="position:relative;top:6px;height: 60px;width:203px;margin-right: 4px;" />
-                        </td>
-                    </tr>
-                </table>
+<body>
+    <div class="body-inner">
+        <div style="width:800px;height:1200px;background-image:url(https://kinder.gpucloud.ru/images/mail-bg.jpg);background-size:cover;">
+            <div style="padding: 249px 117px 0px 172px;">
+                <div style="min-height: 780px;">
+                    <table style="margin:0 0 0 auto;">
+                        <tr style="vertical-align: baseline;">
+                            <td>
+                                <img src="https://kinder.gpucloud.ru/img/promo-logo.png" alt="" style="position:relative;top:6px;height: 60px;width:203px;margin-right: 4px;" />
+                            </td>
+                        </tr>
+                    </table>
 
-                <h1 style="margin: 25px 0;font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;text-shadow: 1px 1px 0px rgba(0, 0, 0), -1px 1px 0px rgba(0, 0, 0), 1px -1px 0px rgba(0, 0, 0), -1px -1px 0px rgba(0, 0, 0), 0px 4px 4px rgba(0, 0, 0, 0.25);text-shadow: none;">
-                    Ваша заявка не прошла<br />
-                    модерацию
-                </h1>
-                <div style="font-size: 17px;line-height: 24px;text-align: center;color: #000000;">
-                    <p style="margin-bottom: 30px;">
-                        К сожалению, Ваша заявка не прошла модерацию.
-                    </p>
-                    <p>
-                        Ознакомьтесь, пожалуйста, с правилами акции.
-                    </p>
-                    <p style="margin-bottom: 30px;">
-                        <a class="btn" href="https://kinder.gpucloud.ru/rules.pdf" style="width:180px;text-decoration:none;font-weight:700;outline:none !important;box-shadow:none;padding:18px 35px;font-family: Noteworthy;font-size: 15px;line-height: 23px;border:none;background: #E54C2E;border-radius: 100px;display: inline-block;position: relative;color:white !important">
-                            ПРАВИЛА АКЦИИ
-                        </a>
-                    </p>
-                    <p>
-                        Попробуйте отправить заявку снова!
-                    </p>
-                    <p>
-                        <a class="btn" href="https://www.kinder.com/ru/ru/kinder-new-year" style="width:180px;text-decoration:none;font-weight:700;outline:none !important;box-shadow:none;padding:18px 35px;font-family: Noteworthy;font-size: 15px;line-height: 23px;border:none;background: #E54C2E;border-radius: 100px;display: inline-block;position: relative;color:white !important">
-                            ОТПРАВИТЬ ЗАЯВКУ
-                        </a>
-                    </p>
+                    <h1 style="margin: 25px 0;font-size: 26px;line-height: 37px;text-align: center;color: #F04E23;text-shadow: 1px 1px 0px rgba(0, 0, 0), -1px 1px 0px rgba(0, 0, 0), 1px -1px 0px rgba(0, 0, 0), -1px -1px 0px rgba(0, 0, 0), 0px 4px 4px rgba(0, 0, 0, 0.25);text-shadow: none;">
+                        Ваша заявка не прошла<br />
+                        модерацию
+                    </h1>
+                    <div style="font-size: 17px;line-height: 24px;text-align: center;color: #000000;">
+                        <p style="margin-bottom: 30px;">
+                            К сожалению, Ваша заявка не прошла модерацию.
+                        </p>
+                        <p>
+                            Ознакомьтесь, пожалуйста, с правилами акции.
+                        </p>
+                        <p style="margin-bottom: 30px;">
+                            <a class="btn" href="https://kinder.gpucloud.ru/rules.pdf" style="width:180px;text-decoration:none;font-weight:700;outline:none !important;box-shadow:none;padding:18px 35px;font-family: Noteworthy;font-size: 15px;line-height: 23px;border:none;background: #E54C2E;border-radius: 100px;display: inline-block;position: relative;color:white !important">
+                                ПРАВИЛА АКЦИИ
+                            </a>
+                        </p>
+                        <p>
+                            Попробуйте отправить заявку снова!
+                        </p>
+                        <p>
+                            <a class="btn" href="https://www.kinder.com/ru/ru/kinder-new-year" style="width:180px;text-decoration:none;font-weight:700;outline:none !important;box-shadow:none;padding:18px 35px;font-family: Noteworthy;font-size: 15px;line-height: 23px;border:none;background: #E54C2E;border-radius: 100px;display: inline-block;position: relative;color:white !important">
+                                ОТПРАВИТЬ ЗАЯВКУ
+                            </a>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div style="text-align: center;">
-            <a href="{{ $unsubscribe }}" style="color: #40160B !important;font-size: 20px;">Отписаться от рассылки</a>
+            <div style="text-align: center;">
+                <a href="{{ $unsubscribe }}" style="color: #40160B !important;font-size: 20px;">Отписаться от рассылки</a>
+            </div>
         </div>
     </div>
 </body>
