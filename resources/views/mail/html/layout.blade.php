0b8ae3e55e9351dbeaef022d5fd0816588cabe78
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-07 18:37:03 +0600

    sdsd

---
+++ resources/views/mail/html/layout.blade.php
@@ -0,0 +1,557 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title>
+    <style>
+        @font-face {
+            font-family: Noteworthy;
+            src: url(https://kinder.gpucloud.ru/fonts/noteworthy-bold.woff2) format("woff2"),
+                url(https://kinder.gpucloud.ru/fonts/noteworthy-bold.woff) format("woff");
+            font-weight: 700;
+            font-style: normal;
+        }
+
+        a.btn:active {
+            top: 1px;
+        }
+
+        * {
+            font-weight: bold;
+        }
+
+        body,
+        html {
+            width: 320px;
+            margin: 0;
+            padding: 0;
+            font-family: Noteworthy;
+            font-weight: bold;
+        }
+
+        body>div {
+            width: 320px;
+            height: 480px;
+            background-image: url(https://kinder.gpucloud.ru/images/mail-bg.jpg);
+            background-size: cover;
+        }
+
+        .body-inner {
+            padding: 99px 46px 0px 68px;
+        }
+
+        .body-inner>div {
+            min-height: 312px;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .top-kinder {
+            position: relative;
+            top: 2px;
+            height: 24px;
+            width: 81px;
+            margin-right: 1px;
+        }
+
+        h1 {
+            margin: 10px 0;
+            font-size: 10px;
+            line-height: 15px;
+            text-align: center;
+            color: #f04e23;
+            text-shadow: 1px 1px 0px rgba(0, 0, 0),
+                -1px 1px 0px rgba(0, 0, 0), 1px -1px 0px rgba(0, 0, 0),
+                -1px -1px 0px rgba(0, 0, 0), 0px 4px 4px rgba(0, 0, 0, 0.25);
+            text-shadow: none;
+        }
+
+        h2 {
+            font-size: 10px;
+            line-height: 15px;
+            margin-bottom: 8px;
+            text-align: center;
+            color: #f04e23;
+        }
+
+        .text-content {
+            font-size: 8px;
+            line-height: 10px;
+            text-align: center;
+            color: #000000;
+        }
+
+        .mb-14 {
+            margin-bottom: 6px;
+        }
+
+        .shop {
+            display: block;
+            width: 67px;
+            border-radius: 8px;
+            background: #fff;
+            overflow: hidden;
+        }
+
+        .shop img {
+            display: block;
+            margin: 0 auto;
+        }
+
+        .shop-1 {
+            margin: 0 6px 4px auto;
+            padding: 2px 0;
+        }
+
+        .shop-1 img {
+            width: 47px;
+            height: 13px;
+        }
+
+        .shop-2 {
+            margin: 0 auto 4px 6px;
+            padding: 4px 0;
+        }
+
+        .shop-2 img {
+            width: 38px;
+            height: 8px;
+        }
+
+        .shop-3 {
+            margin: 0 6px 4px auto;
+            padding: 2px 0;
+        }
+
+        .shop-3 img {
+            width: 48px;
+            height: 13px;
+        }
+
+        .shop-4 {
+            margin: 0 auto 4px 6px;
+            padding: 2px 0;
+        }
+
+        .shop-4 img {
+            width: 47px;
+            height: 13px;
+        }
+
+        .unsubscribe-link {
+            color: #40160b !important;
+            font-size: 10px;
+        }
+
+        @media (min-width: 375px) {
+
+            body,
+            html {
+                width: 375px;
+            }
+
+            body>div {
+                width: 375px;
+                height: 563px;
+            }
+
+            .body-inner {
+                padding: 117px 55px 0px 81px;
+            }
+
+            .body-inner>div {
+                min-height: 366px;
+            }
+
+            .top-kinder {
+                top: 3px;
+                height: 28px;
+                width: 95px;
+                margin-right: 2px;
+            }
+
+            h1 {
+                margin: 12px 0;
+                font-size: 12px;
+                line-height: 17px;
+            }
+
+            h2 {
+                font-size: 12px;
+                line-height: 17px;
+                margin-bottom: 9px;
+            }
+
+            .text-content {
+                font-size: 8px;
+                line-height: 11px;
+            }
+
+            .mb-14 {
+                margin-bottom: 6px;
+            }
+
+            .shop {
+                width: 80px;
+                border-radius: 9px;
+            }
+
+            .shop-1 {
+                margin: 0 7px 5px auto;
+                padding: 2px 0;
+            }
+
+            .shop-1 img {
+                width: 53px;
+                height: 15px;
+            }
+
+            .shop-2 {
+                margin: 0 auto 5px 7px;
+                padding: 5px 0;
+            }
+
+            .shop-2 img {
+                width: 44px;
+                height: 9px;
+            }
+
+            .shop-3 {
+                margin: 0 7px 5px auto;
+                padding: 2px 0;
+            }
+
+            .shop-3 img {
+                width: 56px;
+                height: 15px;
+            }
+
+            .shop-4 {
+                margin: 0 auto 7px 5px;
+                padding: 2px 0;
+            }
+
+            .shop-4 img {
+                width: 53px;
+                height: 14px;
+            }
+
+            .unsubscribe-link {
+                font-size: 9px;
+            }
+        }
+
+        @media (min-width: 414px) {
+
+            body,
+            html {
+                width: 414px;
+            }
+
+            body>div {
+                width: 414px;
+                height: 621px;
+            }
+
+            .body-inner {
+                padding: 128px 51px 0px 89px;
+            }
+
+            .body-inner>div {
+                min-height: 403px;
+            }
+
+            .top-kinder {
+                top: 3px;
+                height: 31px;
+                width: 105px;
+                margin-right: 2px;
+            }
+
+            h1 {
+                margin: 13px 0;
+                font-size: 13px;
+                line-height: 19px;
+            }
+
+            h2 {
+                font-size: 13px;
+                line-height: 19px;
+                margin-bottom: 10px;
+            }
+
+            .text-content {
+                font-size: 9px;
+                line-height: 13px;
+            }
+
+            .mb-14 {
+                margin-bottom: 7px;
+            }
+
+            .shop {
+                width: 88px;
+                border-radius: 10px;
+            }
+
+            .shop-1 {
+                margin: 0 8px 5px auto;
+                padding: 2px 0;
+            }
+
+            .shop-1 img {
+                width: 59px;
+                height: 16px;
+            }
+
+            .shop-2 {
+                margin: 0 auto 5px 8px;
+                padding: 5px 0;
+            }
+
+            .shop-2 img {
+                width: 49px;
+                height: 10px;
+            }
+
+            .shop-3 {
+                margin: 0 8px 5px auto;
+                padding: 2px 0;
+            }
+
+            .shop-3 img {
+                width: 62px;
+                height: 16px;
+            }
+
+            .shop-4 {
+                margin: 0 auto 5px 8px;
+                padding: 3px 0;
+            }
+
+            .shop-4 img {
+                width: 59px;
+                height: 16px;
+            }
+
+            .unsubscribe-link {
+                font-size: 10px;
+            }
+        }
+
+        @media (min-width: 768px) {
+
+            body,
+            html {
+                width: 768px;
+            }
+
+            body>div {
+                width: 768px;
+                height: 1152px;
+            }
+
+            .body-inner {
+                padding: 239px 112px 0px 165px;
+            }
+
+            .body-inner>div {
+                min-height: 749px;
+            }
+
+            .top-kinder {
+                top: 5px;
+                height: 57px;
+                width: 211px;
+                margin-right: 4px;
+            }
+
+            h1 {
+                margin: 24px 0;
+                font-size: 25px;
+                line-height: 35px;
+            }
+
+            h2 {
+                font-size: 25px;
+                line-height: 35px;
+                margin-bottom: 19px;
+            }
+
+            .text-content {
+                font-size: 16px;
+                line-height: 23px;
+            }
+
+            .mb-14 {
+                margin-bottom: 13px;
+            }
+
+            .shop {
+                width: 164px;
+                border-radius: 19px;
+            }
+
+            .shop-1 {
+                margin: 0 14px 9px auto;
+                padding: 4px 0;
+            }
+
+            .shop-1 img {
+                width: 109px;
+                height: 31px;
+            }
+
+            .shop-2 {
+                margin: 0 auto 9px 14px;
+                padding: 9px 0;
+            }
+
+            .shop-2 img {
+                width: 91px;
+                height: 19px;
+            }
+
+            .shop-3 {
+                margin: 0 14px 9px auto;
+                padding: 4px 0;
+            }
+
+            .shop-3 img {
+                width: 115px;
+                height: 31px;
+            }
+
+            .shop-4 {
+                margin: 0 auto 9px 14px;
+                padding: 5px 0;
+            }
+
+            .shop-4 img {
+                width: 109px;
+                height: 30px;
+            }
+
+            .unsubscribe-link {
+                font-size: 19px;
+            }
+        }
+
+        @media (min-width: 800px) {
+            body>div {
+                width: 800px;
+                height: 1200px;
+            }
+
+            .body-inner {
+                padding: 249px 117px 0px 172px;
+            }
+
+            .body-inner>div {
+                min-height: 780px;
+            }
+
+            .top-kinder {
+                top: 6px;
+                height: 60px;
+                width: 203px;
+                margin-right: 4px;
+            }
+
+            h1 {
+                margin: 25px 0;
+                font-size: 26px;
+                line-height: 37px;
+            }
+
+            h2 {
+                font-size: 26px;
+                line-height: 37px;
+                margin-bottom: 20px;
+            }
+
+            .text-content {
+                font-size: 17px;
+                line-height: 24px;
+            }
+
+            .mb-14 {
+                margin-bottom: 14px;
+            }
+
+            .shop {
+                width: 171px;
+                border-radius: 20px;
+            }
+
+            .shop-1 {
+                margin: 0 15px 10px auto;
+                padding: 4px 0;
+            }
+
+            .shop-1 img {
+                width: 114px;
+                height: 32px;
+            }
+
+            .shop-2 {
+                margin: 0 auto 10px 15px;
+                padding: 10px 0;
+            }
+
+            .shop-2 img {
+                width: 95px;
+                height: 20px;
+            }
+
+            .shop-3 {
+                margin: 0 15px 10px auto;
+                padding: 4px 0;
+            }
+
+            .shop-3 img {
+                width: 120px;
+                height: 32px;
+            }
+
+            .shop-4 {
+                margin: 0 auto 10px 15px;
+                padding: 5px 0;
+            }
+
+            .shop-4 img {
+                width: 114px;
+                height: 31px;
+            }
+
+            .unsubscribe-link {
+                font-size: 20px;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <div class="body-inner">
+            <div>
+                <table style="margin:0 0 0 auto;">
+                    <tr style="vertical-align: baseline;">
+                        <td>
+                            <img src="https://kinder.gpucloud.ru/img/promo-logo.png" alt="" class="top-kinder" />
+                        </td>
+                    </tr>
+                </table>
+                @yield('content')
+            </div>
+        </div>
+        <div class="text-center">
+            <a href="{{ $unsubscribe }}" class="unsubscribe-link">Отписаться от рассылки</a>
+        </div>
+    </div>
+</body>
+
+</html>
