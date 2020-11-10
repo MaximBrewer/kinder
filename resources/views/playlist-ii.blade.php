b7dbddf515ce86099f86324a5394784e763ac8d6
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-10 08:01:31 +0600

    sdfsdd

--- resources/views/playlist-ii.blade.php
+++ resources/views/playlist-ii.blade.php
@@ -6,8 +6,4 @@
 {{ $photoChunk }}
 {{ $achieveChunk }}
 {{ $hobbyChunk }}
-#EXTINF:7.808,
-#EXT-X-DISCONTINUITY
-//montage-vod-hls.cdnvideo.ru/montage-vod/_definst_/mp4:montage/kinder/part_v/bicycle.mp4/media_0.ts
 #EXT-X-ENDLIST
-
