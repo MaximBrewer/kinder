51f61590b110450a2a202e2b6bb49ab06281a478
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-10 08:20:37 +0600

    sds

--- resources/views/playlist-i.blade.php
+++ resources/views/playlist-i.blade.php
@@ -26,4 +26,11 @@
 {{ $photoChunk }}
 {{ $achieveChunk }}
 {{ $hobbyChunk }}
+#EXTINF:10.8,
+#EXT-X-DISCONTINUITY
+https://montage-vod-hls.cdnvideo.ru/montage-vod/_definst_/mp4:montage/kinder/part_vii/all%20%281280xauto%29.mp4/media_0.ts
+#EXTINF:10.8,
+https://montage-vod-hls.cdnvideo.ru/montage-vod/_definst_/mp4:montage/kinder/part_vii/all%20%281280xauto%29.mp4/media_1.ts
+#EXTINF:3.301,
+https://montage-vod-hls.cdnvideo.ru/montage-vod/_definst_/mp4:montage/kinder/part_vii/all%20%281280xauto%29.mp4/media_2.ts
 #EXT-X-ENDLIST
