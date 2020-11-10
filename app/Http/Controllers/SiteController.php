4340fe469d5194198503fe0632a3ddfc17767d30
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-10 07:42:38 +0600

    sds

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -139,7 +139,7 @@ class SiteController extends Controller
                 if (strstr($val, "#EXTINF")) {
                     $next = true;
                     $duration = trim(',', explode(":", $val)[1]);
-                    var_dump(explode(":", $val));
+                    var_dump(str_replace(["#EXTINF:", ","], "", $val));
                 }
             }
             var_dump($chunks);
