71168d50dd3519a735d0df50c4fbd9db2af54d64
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-10 09:08:01 +0600

    sdsd

--- resources/js/video.js
+++ resources/js/video.js
@@ -1,5 +1,4 @@
 var step = 1;
-
 var player = videojs(
     "video",
     {
@@ -24,148 +23,35 @@ var player = videojs(
                     "scale(2)";
                 document.getElementById("photoFrame").style.transition =
                     "10s linear";
+                document.getElementById("photoFrame").style.backgroundImage =
+                    "url(" + photo + ")";
+                document.getElementById("photoFrame").style.backgroundImage =
+                    "url(" + photo + ")";
                 if (photo) document.getElementById("hover").style.zIndex = "10";
-                that.src({
-                    src: "/playlist-ii/" + hash + ".m3u8",
-                    type: "application/x-mpegURL"
-                });
-                that.play();
+                // that.src({
+                //     src: "/playlist-ii/" + hash + ".m3u8",
+                //     type: "application/x-mpegURL"
+                // });
+                // that.play();
             }
             if (step == 3) {
+                // document.getElementById("photoFrame").style.transform =
+                //     "scale(2)";
+                // document.getElementById("photoFrame").style.transition =
+                //     "10s linear";
+                // if (photo) document.getElementById("hover").style.zIndex = "10";
                 // that.src({
-                //     type: "application/x-mpegURL",
-                //     src: "/playlist-ii/" + hash + ".m3u8"
+                //     src: "/playlist-ii/" + hash + ".m3u8",
+                //     type: "application/x-mpegURL"
                 // });
             }
         });
         this.on("canplay", function() {
             if (step == 2) {
-                setTimeout(function() {
-                    document.getElementById("hover").style.zIndex = "-1";
-                }, photo_duration * 1000);
+                // setTimeout(function() {
+                //     document.getElementById("hover").style.zIndex = "-1";
+                // }, photo_duration * 1000);
             }
-        });
-        this.on("loadedmetadata", function() {
-            // if (step == 2) {
-            //     setTimeout(function() {
-            //         that.play();
-            //         setTimeout(function() {
-            //             document.getElementById("hover").style.zIndex = "-1";
-            //         }, photo_duration * 1000);
-            //     }, 1200);
-            // }
-        });
-        this.on("play", function() {
-            console.log("play", step);
-            window.scrollTo(0, 1);
-        });
-        this.on("firstplay", function() {
-            console.log("firstplay", that);
-        });
-        this.on("change", function() {
-            console.log("change", that);
-        });
-        this.on("sourceended", function() {
-            console.log("sourceended", that);
-        });
-        this.on("done", function() {
-            console.log("done", that);
-        });
-        this.on("selectedinitialmedia", function() {
-            console.log("selectedinitialmedia", that);
-        });
-        this.on("sourceopen", function() {
-            console.log("sourceopen", that);
-        });
-        this.on("loadedmetadata", function() {
-            console.log("loadedmetadata", that);
-        });
-        this.on("loadedplaylist", function() {
-            console.log("loadedplaylist", that);
-        });
-        this.on("renditiondisabled", function() {
-            console.log("renditiondisabled", that);
-        });
-        this.on("bandwidthupdate", function() {
-            console.log("bandwidthupdate", that);
-        });
-        this.on("audioinfo", function() {
-            console.log("audioinfo", that);
-        });
-        this.on("mediachanging", function() {
-            console.log("mediachanging", that);
-        });
-        this.on("mediachange", function() {
-            console.log("mediachange", that);
-        });
-        this.on("playlistunchanged", function() {
-            console.log("playlistunchanged", that);
-        });
-        this.on("renditiondisabled", function() {
-            console.log("renditiondisabled", that);
-        });
-        this.on("renditionenabled", function() {
-            console.log("renditionenabled", that);
-        });
-        this.on("progress", function(e, c) {
-            console.log("progress", e, c);
-        });
-        this.on("syncinfoupdate", function() {
-            console.log("syncinfoupdate", that);
-        });
-        this.on("timestampoffset", function() {
-            console.log("timestampoffset", that);
-        });
-        this.on("earlyabort", function() {
-            console.log("earlyabort", that);
-        });
-        this.on("seekablechanged", function() {
-            console.log("seekablechanged", that);
-        });
-        this.on("waiting", function() {
-            console.log("waiting", that);
-        });
-        this.on("canplay", function() {
-            console.log("canplay", that);
-        });
-        this.on("mediaupdatetimeout", function() {
-            console.log("mediaupdatetimeout", that);
-        });
-        this.on("dispose", function() {
-            console.log("dispose", that);
-        });
-        this.on("data", function() {
-            console.log("data", that);
-        });
-        this.on("timestamp", function() {
-            console.log("timestamp", that);
-        });
-        this.on("timelineStartInfo", function() {
-            console.log("timelineStartInfo", that);
-        });
-        this.on("baseMediaDecodeTime", function() {
-            console.log("baseMediaDecodeTime", that);
-        });
-        this.on("seeking", function() {
-            console.log("seeking", that);
-        });
-        this.on("seeked", function() {
-            console.log("seeked", that);
-        });
-        this.on("sourceclose", function() {
-            console.log("sourceclose", that);
-        });
-        this.on("sourceclose", function() {
-            console.log("sourceclose", that);
-        });
-        this.on("sourceclose", function() {
-            console.log("sourceclose", that);
-        });
-        this.on("sourceclose", function() {
-            console.log("sourceclose", that);
-        });
-        this.on("sourceclose", function() {
-            console.log("sourceclose", that);
         });
     }
 );
