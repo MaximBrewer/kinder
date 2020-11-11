9dabe5641a38e608250f95127f1c4da0b9edb3ce
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-12 05:10:54 +0600

    sd

--- public/js/video.js
+++ public/js/video.js
@@ -1,1 +1,368 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var d=t[o]={i:o,l:!1,exports:{}};return e[o].call(d.exports,d,d.exports,n),d.l=!0,d.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var d in e)n.d(o,d,function(t){return e[t]}.bind(null,d));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=109)}({109:function(e,t,n){e.exports=n(110)},110:function(e,t){var n=1,o=function(e){e.target.style.opacity="1","1"==document.getElementById("gftI").style.opacity&&"1"==document.getElementById("gftII").style.opacity&&"1"==document.getElementById("gftIII").style.opacity&&(r.src({src:"/playlist-iv/"+hash+".m3u8",type:"application/x-mpegURL"}),n=6,r.play())},d=function(e){var t="red",o=e.changedTouches?e.changedTouches[0].clientX:e.clientX;t=.375*window.innerWidth>o?"gold":window.innerWidth-.375*window.innerWidth>o?"red":"silver",n=4,r.src({src:"/playlist-iii/"+t+".m3u8",type:"application/x-mpegURL"}),r.play()},r=videojs("video",{sources:[{src:"/playlist-i/"+hash+".m3u8",type:"application/x-mpegURL"}],controls:!0,control:!0,poster:"https://montage-cache.cdnvideo.ru/montage/.previews/preview-5fa973f50e47cf6eac7d7b7a.jpg"},(function(){var e=this;this.on("ended",(function(){++n,console.log("ended",e,n),2==n&&(photo&&(document.getElementById("photoFrame").style.backgroundImage="url("+photo+")",document.getElementById("hover").style.zIndex="10",document.getElementById("hover").style.opacity="1"),document.getElementById("photoFrame").style.transform="scale(1.4)",document.getElementById("photoFrame").style.transition="transform 10s linear",document.getElementById("photoFrame").style.backgroundRepeat="no-repeat",document.getElementById("photoFrame").style.backgroundSize="auto 92%",document.getElementById("photoFrame").style.backgroundPosition="top left 57%",e.src({src:"/playlist-ii/"+hash+".m3u8",type:"application/x-mpegURL"}),e.play()),3==n&&(document.getElementById("hover").style.opacity="0",document.getElementById("hover").style.zIndex="-1",document.getElementById("hover2").style.opacity="1",document.getElementById("hover2").style.zIndex="10",document.getElementById("photoFrame2").style.transform="scale(1.2)",document.getElementById("photoFrame2").style.transition="transform 10s linear",document.getElementById("photoFrame2").style.backgroundRepeat="no-repeat",document.getElementById("photoFrame2").style.backgroundSize="auto 92%",document.getElementById("photoFrame2").style.backgroundPosition="top left 57%",e.src({src:"https://montage-vod-hls.cdnvideo.ru/montage-vod/_definst_/mp4:montage/kinder/part_viii/all%20%281280xauto%29.mp4/playlist.m3u8",type:"application/x-mpegURL"}),e.play(),document.getElementById("photoFrame2").addEventListener("click",d),document.getElementById("photoFrame2").addEventListener("touchstart",d)),5==n&&(document.getElementById("hover").style.opacity="0",document.getElementById("hover").style.zIndex="-1",document.getElementById("hover2").style.opacity="0",document.getElementById("hover2").style.zIndex="-1",document.getElementById("hover3").style.zIndex="10",document.getElementById("hover3").style.opacity="1",document.getElementById("photoFrame3").style.transform="scale(1.2)",document.getElementById("photoFrame3").style.transition="transform 10s linear",document.getElementById("photoFrame3").style.backgroundRepeat="no-repeat",document.getElementById("photoFrame3").style.backgroundSize="auto 92%",document.getElementById("photoFrame2").style.backgroundPosition="top left 57%",e.src({src:"https://montage-cache.cdnvideo.ru/montage/kinder/part_xi/voice.mp3",type:"audio/mpeg"}),e.play(),document.getElementById("gftI").addEventListener("touchstart",o),document.getElementById("gftI").addEventListener("click",o),document.getElementById("gftII").addEventListener("touchstart",o),document.getElementById("gftII").addEventListener("click",o),document.getElementById("gftIII").addEventListener("touchstart",o),document.getElementById("gftIII").addEventListener("click",o))})),this.on("canplay",(function(){n>=2&&setTimeout((function(){document.getElementById("hover").style.zIndex="-1",document.getElementById("hover").style.opacity="0"}),1e3*photo_duration+1500),n>=4&&(document.getElementById("hover2").style.zIndex="-1",document.getElementById("hover2").style.opacity="0"),n>=6&&(document.getElementById("hover3").style.zIndex="-1",document.getElementById("hover3").style.opacity="0")}))}));var a=document.getElementsByTagName("body")[0];a.addEventListener("mouseup",(function(e){})),a.addEventListener("touchstart",(function(e){})),a.addEventListener("touchend",(function(e){r.play()})),window.addEventListener("message",(function(e){"start"==e.data?(console.log(),e.source.postMessage("start:"+r.currentTime(),"*")):(e.data="end")&&e.source.postMessage("end:"+r.currentTime(),"*")}))}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/video.js":
+/*!*******************************!*\
+  !*** ./resources/js/video.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var step = 1;
+
+var chooseGift = function chooseGift(e) {
+  e.target.style.opacity = "1";
+
+  if (document.getElementById("gftI").style.opacity == "1" && document.getElementById("gftII").style.opacity == "1" && document.getElementById("gftIII").style.opacity == "1") {
+    player.src({
+      src: "/playlist-iv/" + hash + ".m3u8",
+      type: "application/x-mpegURL"
+    });
+    step = 6;
+    player.play();
+  }
+};
+
+var chooseBall = function chooseBall(e) {
+  var color = "red";
+  var clientX = e.changedTouches ? e.changedTouches[0].clientX : e.clientX;
+  window.innerWidth * 0.375 > clientX ? color = "gold" : window.innerWidth - window.innerWidth * 0.375 > clientX ? color = "red" : color = "silver";
+  step = 4;
+  player.src({
+    src: "/playlist-iii/" + color + ".m3u8",
+    type: "application/x-mpegURL"
+  });
+  player.play();
+};
+
+var player = videojs("video", {
+  sources: [{
+    src: "/playlist-i/" + hash + ".m3u8",
+    type: "application/x-mpegURL"
+  }],
+  controls: true,
+  control: true,
+  poster: "https://montage-cache.cdnvideo.ru/montage/.previews/preview-5fa973f50e47cf6eac7d7b7a.jpg"
+}, function () {
+  var that = this;
+  this.on("ended", function () {
+    ++step;
+    console.log("ended", that, step);
+
+    if (step == 2) {
+      if (photo) {
+        document.getElementById("photoFrame").style.backgroundImage = "url(" + photo + ")";
+        document.getElementById("hover").style.zIndex = "10";
+        document.getElementById("hover").style.opacity = "1";
+      }
+
+      document.getElementById("photoFrame").style.transform = "scale(1.4)";
+      document.getElementById("photoFrame").style.transition = "transform 10s linear";
+      document.getElementById("photoFrame").style.backgroundRepeat = "no-repeat";
+      document.getElementById("photoFrame").style.backgroundSize = "auto 92%";
+      document.getElementById("photoFrame").style.backgroundPosition = "top left 57%";
+      that.src({
+        src: "/playlist-ii/" + hash + ".m3u8",
+        type: "application/x-mpegURL"
+      });
+      that.play();
+    }
+
+    if (step == 3) {
+      document.getElementById("hover").style.opacity = "0";
+      document.getElementById("hover").style.zIndex = "-1";
+      document.getElementById("hover2").style.opacity = "1";
+      document.getElementById("hover2").style.zIndex = "10";
+      document.getElementById("photoFrame2").style.transform = "scale(1.2)";
+      document.getElementById("photoFrame2").style.transition = "transform 10s linear";
+      document.getElementById("photoFrame2").style.backgroundRepeat = "no-repeat";
+      document.getElementById("photoFrame2").style.backgroundSize = "auto 92%";
+      document.getElementById("photoFrame2").style.backgroundPosition = "top left 57%";
+      that.src({
+        src: "https://montage-vod-hls.cdnvideo.ru/montage-vod/_definst_/mp4:montage/kinder/part_viii/all%20%281280xauto%29.mp4/playlist.m3u8",
+        type: "application/x-mpegURL"
+      });
+      that.play();
+      document.getElementById("photoFrame2").addEventListener("click", chooseBall);
+      document.getElementById("photoFrame2").addEventListener("touchstart", chooseBall);
+    }
+
+    if (step == 5) {
+      document.getElementById("hover").style.opacity = "0";
+      document.getElementById("hover").style.zIndex = "-1";
+      document.getElementById("hover2").style.opacity = "0";
+      document.getElementById("hover2").style.zIndex = "-1";
+      document.getElementById("hover3").style.zIndex = "10";
+      document.getElementById("hover3").style.opacity = "1";
+      document.getElementById("photoFrame3").style.transform = "scale(1.2)";
+      document.getElementById("photoFrame3").style.transition = "transform 10s linear";
+      document.getElementById("photoFrame3").style.backgroundRepeat = "no-repeat";
+      document.getElementById("photoFrame3").style.backgroundSize = "auto 92%";
+      document.getElementById("photoFrame2").style.backgroundPosition = "top left 57%";
+      that.src({
+        src: "https://montage-cache.cdnvideo.ru/montage/kinder/part_xi/voice.mp3",
+        type: "audio/mpeg"
+      });
+      that.play();
+      document.getElementById("gftI").addEventListener("touchstart", chooseGift);
+      document.getElementById("gftI").addEventListener("click", chooseGift);
+      document.getElementById("gftII").addEventListener("touchstart", chooseGift);
+      document.getElementById("gftII").addEventListener("click", chooseGift);
+      document.getElementById("gftIII").addEventListener("touchstart", chooseGift);
+      document.getElementById("gftIII").addEventListener("click", chooseGift);
+    }
+  });
+  this.on("canplay", function () {
+    if (step >= 2) {
+      setTimeout(function () {
+        document.getElementById("hover").style.zIndex = "-1";
+        document.getElementById("hover").style.opacity = "0";
+      }, photo_duration * 1000 + 1500);
+    }
+
+    if (step >= 4) {
+      document.getElementById("hover2").style.zIndex = "-1";
+      document.getElementById("hover2").style.opacity = "0";
+    }
+
+    if (step >= 6) {
+      document.getElementById("hover3").style.zIndex = "-1";
+      document.getElementById("hover3").style.opacity = "0";
+    }
+  });
+  this.on("play", function () {
+    console.log("play", step);
+    window.scrollTo(0, 1);
+  });
+  this.on("firstplay", function () {
+    console.log("firstplay", that);
+  });
+  this.on("change", function () {
+    console.log("change", that);
+  });
+  this.on("sourceended", function () {
+    console.log("sourceended", that);
+  });
+  this.on("done", function () {
+    console.log("done", that);
+  });
+  this.on("selectedinitialmedia", function () {
+    console.log("selectedinitialmedia", that);
+  });
+  this.on("sourceopen", function () {
+    console.log("sourceopen", that);
+  });
+  this.on("loadedmetadata", function () {
+    console.log("loadedmetadata", that);
+  });
+  this.on("loadedplaylist", function () {
+    console.log("loadedplaylist", that);
+  });
+  this.on("renditiondisabled", function () {
+    console.log("renditiondisabled", that);
+  });
+  this.on("bandwidthupdate", function () {
+    console.log("bandwidthupdate", that);
+  });
+  this.on("audioinfo", function () {
+    console.log("audioinfo", that);
+  });
+  this.on("mediachanging", function () {
+    console.log("mediachanging", that);
+  });
+  this.on("mediachange", function () {
+    console.log("mediachange", that);
+  });
+  this.on("playlistunchanged", function () {
+    console.log("playlistunchanged", that);
+  });
+  this.on("renditiondisabled", function () {
+    console.log("renditiondisabled", that);
+  });
+  this.on("renditionenabled", function () {
+    console.log("renditionenabled", that);
+  });
+  this.on("progress", function () {
+    console.log("progress", that);
+  });
+  this.on("syncinfoupdate", function () {
+    console.log("syncinfoupdate", that);
+  });
+  this.on("timestampoffset", function () {
+    console.log("timestampoffset", that);
+  });
+  this.on("earlyabort", function () {
+    console.log("earlyabort", that);
+  });
+  this.on("seekablechanged", function () {
+    console.log("seekablechanged", that);
+  });
+  this.on("waiting", function () {
+    console.log("waiting", that);
+  });
+  this.on("canplay", function () {
+    console.log("canplay", that);
+  });
+  this.on("mediaupdatetimeout", function () {
+    console.log("mediaupdatetimeout", that);
+  });
+  this.on("dispose", function () {
+    console.log("dispose", that);
+  });
+  this.on("data", function () {
+    console.log("data", that);
+  });
+  this.on("timestamp", function () {
+    console.log("timestamp", that);
+  });
+  this.on("timelineStartInfo", function () {
+    console.log("timelineStartInfo", that);
+  });
+  this.on("baseMediaDecodeTime", function () {
+    console.log("baseMediaDecodeTime", that);
+  });
+  this.on("seeking", function () {
+    console.log("seeking", that);
+  });
+  this.on("seeked", function () {
+    console.log("seeked", that);
+  });
+  this.on("sourceclose", function () {
+    console.log("sourceclose", that);
+  });
+  this.on("sourceclose", function () {
+    console.log("sourceclose", that);
+  });
+  this.on("sourceclose", function () {
+    console.log("sourceclose", that);
+  });
+  this.on("sourceclose", function () {
+    console.log("sourceclose", that);
+  });
+  this.on("sourceclose", function () {
+    console.log("sourceclose", that);
+  });
+});
+
+function togglePlayer() {
+  // player.paused() ?
+  player.play(); // : player.pause();
+}
+
+var body = document.getElementsByTagName("body")[0];
+body.addEventListener("mouseup", function (event) {// togglePlayer();
+});
+body.addEventListener("touchstart", function (event) {// togglePlayer();
+});
+body.addEventListener("touchend", function (event) {
+  togglePlayer();
+});
+window.addEventListener("message", function (event) {
+  if (event.data == "start") {
+    console.log();
+    event.source.postMessage("start:" + player.currentTime(), "*");
+  } else if (event.data = "end") {
+    event.source.postMessage("end:" + player.currentTime(), "*");
+  }
+});
+
+/***/ }),
+
+/***/ 1:
+/*!*************************************!*\
+  !*** multi ./resources/js/video.js ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /Users/maxim/Projects/kinder/resources/js/video.js */"./resources/js/video.js");
+
+
+/***/ })
+
+/******/ });
