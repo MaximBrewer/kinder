e7f9faa5363c53eaf0d3308f6dbcda19c4182fd7
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-12 22:36:44 +0600

    sdsd

--- resources/views/chunks/part_i/boy/640.blade.php
+++ resources/views/chunks/part_i/boy/640.blade.php
@@ -1,14 +1,14 @@
 #EXTINF:10.8,
-{{ $cdn }}/part_i/boy%20%28640xauto%29.mp4/media_0.ts
+{{ $cdn }}part_i/p1boy%20%28640xauto%29.mp4/media_0.ts
 #EXTINF:10.8,
-{{ $cdn }}/part_i/boy%20%28640xauto%29.mp4/media_1.ts
+{{ $cdn }}part_i/p1boy%20%28640xauto%29.mp4/media_1.ts
 #EXTINF:10.8,
-{{ $cdn }}/part_i/boy%20%28640xauto%29.mp4/media_2.ts
+{{ $cdn }}part_i/p1boy%20%28640xauto%29.mp4/media_2.ts
 #EXTINF:10.8,
-{{ $cdn }}/part_i/boy%20%28640xauto%29.mp4/media_3.ts
+{{ $cdn }}part_i/p1boy%20%28640xauto%29.mp4/media_3.ts
 #EXTINF:10.8,
-{{ $cdn }}/part_i/boy%20%28640xauto%29.mp4/media_4.ts
+{{ $cdn }}part_i/p1boy%20%28640xauto%29.mp4/media_4.ts
 #EXTINF:10.8,
-{{ $cdn }}/part_i/boy%20%28640xauto%29.mp4/media_5.ts
+{{ $cdn }}part_i/p1boy%20%28640xauto%29.mp4/media_5.ts
 #EXTINF:6.8,
-{{ $cdn }}/part_i/boy%20%28640xauto%29.mp4/media_6.ts
+{{ $cdn }}part_i/p1boy%20%28640xauto%29.mp4/media_6.ts
