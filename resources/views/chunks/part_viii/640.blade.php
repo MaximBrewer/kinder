98e3ad86924254558a3740cb448ed9efaf3ec9be
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-13 21:02:51 +0600

    sdsd

--- resources/views/chunks/part_viii/640.blade.php
+++ resources/views/chunks/part_viii/640.blade.php
@@ -1,7 +1,7 @@
 #EXT-X-DISCONTINUITY
 #EXTINF:10.8,
-{{ $cdn }}part_viii/p8%20%28640xauto%29.mp4/media_0.ts
+{{ $cdn }}part_viii/pt8%20%28640xauto%29.mp4/media_0.ts
 #EXTINF:10.8,
-{{ $cdn }}part_viii/p8%20%28640xauto%29.mp4/media_1.ts
+{{ $cdn }}part_viii/pt8%20%28640xauto%29.mp4/media_1.ts
 #EXTINF:0.84,
-{{ $cdn }}part_viii/p8%20%28640xauto%29.mp4/media_2.ts
+{{ $cdn }}part_viii/pt8%20%28640xauto%29.mp4/media_2.ts
