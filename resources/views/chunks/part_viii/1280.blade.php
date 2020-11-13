cbc24c0f108d4f23faaeb28c31971a7319ac2660
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-13 21:26:36 +0600

    sds

--- resources/views/chunks/part_viii/1280.blade.php
+++ resources/views/chunks/part_viii/1280.blade.php
@@ -1,3 +1,3 @@
 #EXT-X-DISCONTINUITY
-#EXTINF:10,
+#EXTINF:10.027,
 {{ $cdn }}part_viii/pt8%20%281280xauto%29.mp4/media_0.ts
--- resources/views/chunks/part_viii/640.blade.php
+++ resources/views/chunks/part_viii/640.blade.php
@@ -1,3 +1,3 @@
 #EXT-X-DISCONTINUITY
-#EXTINF:10,
+#EXTINF:10.027,
 {{ $cdn }}part_viii/pt8%20%28640xauto%29.mp4/media_0.ts
