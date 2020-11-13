0d504168375ac29ac6cf7ca7331f02b862a9d261
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-13 21:40:39 +0600

    sdsd

--- resources/views/chunks/part_xv/1024.blade.php
+++ resources/views/chunks/part_xv/1024.blade.php
@@ -1,7 +1,7 @@
 #EXT-X-DISCONTINUITY
 #EXTINF:12,
-{{ $cdn }}part_xv/p15%20%281024xauto%29.mp4/media_0.ts
+{{ $cdn }}part_xv/pt15%20%281024xauto%29.mp4/media_0.ts
 #EXTINF:12,
-{{ $cdn }}part_xv/p15%20%281024xauto%29.mp4/media_1.ts
+{{ $cdn }}part_xv/pt15%20%281024xauto%29.mp4/media_1.ts
 #EXTINF:3.033,
-{{ $cdn }}part_xv/p15%20%281024xauto%29.mp4/media_2.ts
+{{ $cdn }}part_xv/pt15%20%281024xauto%29.mp4/media_2.ts
--- resources/views/chunks/part_xv/1280.blade.php
+++ resources/views/chunks/part_xv/1280.blade.php
@@ -1,7 +1,7 @@
 #EXT-X-DISCONTINUITY
 #EXTINF:10.8,
-{{ $cdn }}part_xv/p15%20%281280xauto%29.mp4/media_0.ts
+{{ $cdn }}part_xv/pt15%20%281280xauto%29.mp4/media_0.ts
 #EXTINF:10.8,
-{{ $cdn }}part_xv/p15%20%281280xauto%29.mp4/media_1.ts
+{{ $cdn }}part_xv/pt15%20%281280xauto%29.mp4/media_1.ts
 #EXTINF:5.44,
-{{ $cdn }}part_xv/p15%20%281280xauto%29.mp4/media_2.ts
+{{ $cdn }}part_xv/pt15%20%281280xauto%29.mp4/media_2.ts
