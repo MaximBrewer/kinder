3d7599554d526253c90f1fe0d7b5fd6c2287bf51
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-13 21:08:46 +0600

    sds

--- resources/views/chunks/part_viii/1024.blade.php
+++ resources/views/chunks/part_viii/1024.blade.php
@@ -1,5 +1,3 @@
 #EXT-X-DISCONTINUITY
-#EXTINF:12,
-{{ $cdn }}part_viii/p8%20%281024xauto%29.mp4/media_0.ts
-#EXTINF:10.43,
-{{ $cdn }}part_viii/p8%20%281024xauto%29.mp4/media_2.ts
+#EXTINF:10
+{{ $cdn }}part_viii/pt8%20%281024xauto%29.mp4/media_0.ts
--- resources/views/chunks/part_viii/640.blade.php
+++ resources/views/chunks/part_viii/640.blade.php
@@ -1,7 +1,3 @@
 #EXT-X-DISCONTINUITY
-#EXTINF:10.8,
+#EXTINF:10,
 {{ $cdn }}part_viii/pt8%20%28640xauto%29.mp4/media_0.ts
-#EXTINF:10.8,
-{{ $cdn }}part_viii/pt8%20%28640xauto%29.mp4/media_1.ts
-#EXTINF:0.84,
-{{ $cdn }}part_viii/pt8%20%28640xauto%29.mp4/media_2.ts
