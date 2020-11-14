561b64b80895b2bd06e9a62664bb9b18fb7456f7
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-14 18:43:39 +0600

    sdsd

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -22,12 +22,12 @@
 </head>
 
 <body style="margin:0;padding:0;">
+    <audio id="audio" style="opacity:0;z-index:-1;position:absolute;top:0;left:0;width:1px;height:1px;"></audio>
     <video id="video" class="custom-template video-js video-addon-js vjs-default-skin vjs-big-play-centered vjs-nofull" webkit-playsinline playsinline>
         <p class="vjs-no-js">
             Для просмотра включите яваскрипт или обновите браузер до <a href="http://videojs.com/html5-video-support/" target="_blank">совместимых с HTML5 видео</a>
         </p>
     </video>
-    <audio id="audio" controls></audio>
     <script>
         var hash = "{{ $hash }}",
             photo = "{{ $photo }}",
