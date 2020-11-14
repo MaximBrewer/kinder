fe321001a4374d033e5a56ad8b01c2d842ab2583
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-14 21:53:21 +0600

    sdsd

---
+++ resources/views/chunks/part_i/girl/1920.blade.php
@@ -0,0 +1,16 @@
+#EXTINF:10.0,
+{{ $cdn }}part_i/pt1boy%20%281920xauto%29.mp4/media_0.ts
+#EXTINF:10.0,
+{{ $cdn }}part_i/pt1boy%20%281920xauto%29.mp4/media_1.ts
+#EXTINF:10.0,
+{{ $cdn }}part_i/pt1boy%20%281920xauto%29.mp4/media_2.ts
+#EXTINF:10.0,
+{{ $cdn }}part_i/pt1boy%20%281920xauto%29.mp4/media_3.ts
+#EXTINF:10.0,
+{{ $cdn }}part_i/pt1boy%20%281920xauto%29.mp4/media_4.ts
+#EXTINF:10.0,
+{{ $cdn }}part_i/pt1boy%20%281920xauto%29.mp4/media_5.ts
+#EXTINF:10.0,
+{{ $cdn }}part_i/pt1boy%20%281920xauto%29.mp4/media_6.ts
+#EXTINF:1.6,
+{{ $cdn }}part_i/pt1boy%20%281920xauto%29.mp4/media_6.ts
