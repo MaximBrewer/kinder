949efe5ad2c9ef5b66ee022da5697ec69dc0f3c2
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-14 10:30:58 +0600

    sds

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -22,17 +22,7 @@
 </head>
 
 <body style="margin:0;padding:0;">
-    <video id="videoS" class="custom-template video-js video-addon-js vjs-default-skin vjs-big-play-centered vjs-nofull" webkit-playsinline playsinline>
-        <p class="vjs-no-js">
-            Для просмотра включите яваскрипт или обновите браузер до <a href="http://videojs.com/html5-video-support/" target="_blank">совместимых с HTML5 видео</a>
-        </p>
-    </video>
-    <video id="videoG" style="display:none;" class="custom-template video-js video-addon-js vjs-default-skin vjs-big-play-centered vjs-nofull" webkit-playsinline playsinline>
-        <p class="vjs-no-js">
-            Для просмотра включите яваскрипт или обновите браузер до <a href="http://videojs.com/html5-video-support/" target="_blank">совместимых с HTML5 видео</a>
-        </p>
-    </video>
-    <video id="videoB" style="display:none;" class="custom-template video-js video-addon-js vjs-default-skin vjs-big-play-centered vjs-nofull" webkit-playsinline playsinline>
+    <video id="video" class="custom-template video-js video-addon-js vjs-default-skin vjs-big-play-centered vjs-nofull" webkit-playsinline playsinline>
         <p class="vjs-no-js">
             Для просмотра включите яваскрипт или обновите браузер до <a href="http://videojs.com/html5-video-support/" target="_blank">совместимых с HTML5 видео</a>
         </p>
