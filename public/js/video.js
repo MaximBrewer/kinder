0fe91b9b0dad377555c483e485c8636b674bda52
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-14 15:11:57 +0600

    sds

--- public/js/video.js
+++ public/js/video.js
@@ -93,7 +93,309 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/maxim/Projects/kinder/resources/js/video.js: Unexpected token, expected \",\" (184:85)\n\n\u001b[0m \u001b[90m 182 | \u001b[39m    )\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 183 | \u001b[39m    \u001b[90m// player.tech({ IWillNotUseThisInPlugins: true }).hls.resyncLoader();resetEverything\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 184 | \u001b[39m    console\u001b[33m.\u001b[39mlog(player\u001b[33m.\u001b[39mtech({ \u001b[33mIWillNotUseThisInPlugins\u001b[39m\u001b[33m:\u001b[39m \u001b[36mtrue\u001b[39m })\u001b[33m.\u001b[39mhls\u001b[33m.\u001b[39mmainSegmentLoader\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m     | \u001b[39m                                                                                     \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 185 | \u001b[39m    console\u001b[33m.\u001b[39mlog(player\u001b[33m.\u001b[39mtech({ \u001b[33mIWillNotUseThisInPlugins\u001b[39m\u001b[33m:\u001b[39m \u001b[36mtrue\u001b[39m })\u001b[33m.\u001b[39mhls\u001b[33m.\u001b[39mmainSegmentLoader())\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 186 | \u001b[39m    player\u001b[33m.\u001b[39mtrigger(\u001b[32m\"mediachange\"\u001b[39m)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 187 | \u001b[39m    player\u001b[33m.\u001b[39mtech({ \u001b[33mIWillNotUseThisInPlugins\u001b[39m\u001b[33m:\u001b[39m \u001b[36mtrue\u001b[39m })\u001b[33m.\u001b[39mtrigger(\u001b[32m\"mediachange\"\u001b[39m)\u001b[33m;\u001b[39m\u001b[0m\n    at Object._raise (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:766:17)\n    at Object.raiseWithData (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:759:17)\n    at Object.raise (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:753:17)\n    at Object.unexpected (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:8966:16)\n    at Object.expect (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:8952:28)\n    at Object.parseCallExpressionArguments (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:10033:14)\n    at Object.parseCoverCallAndAsyncArrowHead (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9960:29)\n    at Object.parseSubscript (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9896:19)\n    at Object.parseSubscripts (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9867:19)\n    at Object.parseExprSubscripts (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9850:17)\n    at Object.parseUpdate (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9824:21)\n    at Object.parseMaybeUnary (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9813:17)\n    at Object.parseExprOps (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9683:23)\n    at Object.parseMaybeConditional (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9657:23)\n    at Object.parseMaybeAssign (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9620:21)\n    at Object.parseExpressionBase (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9564:23)\n    at /Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9558:39\n    at Object.allowInAnd (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:11297:16)\n    at Object.parseExpression (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9558:17)\n    at Object.parseStatementContent (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:11562:23)\n    at Object.parseStatement (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:11431:17)\n    at Object.parseBlockOrModuleBlockBody (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:12013:25)\n    at Object.parseBlockBody (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:11999:10)\n    at Object.parseBlock (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:11983:10)\n    at Object.parseFunctionBody (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:10963:24)\n    at Object.parseFunctionBodyAndFinish (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:10946:10)\n    at /Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:12153:12\n    at Object.withTopicForbiddingContext (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:11272:14)\n    at Object.parseFunction (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:12152:10)\n    at Object.parseFunctionOrFunctionSent (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:10377:17)\n    at Object.parseExprAtom (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:10202:21)\n    at Object.parseExprAtom (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:4718:20)\n    at Object.parseExprSubscripts (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9844:23)\n    at Object.parseUpdate (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9824:21)\n    at Object.parseMaybeUnary (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9813:17)\n    at Object.parseExprOps (/Users/maxim/Projects/kinder/node_modules/@babel/parser/lib/index.js:9683:23)");
+var resolution = 1280;
+var balls = false;
+var redImg, whiteImg, goldImg;
+var options = {
+  hls: {
+    maxBufferLength: 3
+  }
+};
+
+videojs.Hls.xhr.beforeRequest = function () {
+  return false;
+};
+
+var checkTimeouts = function checkTimeouts() {
+  var ct = player.currentTime();
+  clearTimeout(timeoutBall);
+  clearTimeout(timeoutPhoto);
+  clearTimeout(timeoutGifts);
+
+  if (photo) {
+    if (ct < tp) {
+      timeoutPhoto = setTimeout(function () {
+        setPhoto();
+      }, (tp - ct) * 1000 - 350);
+    } else if (ct < tp + part_iv_duration - 350) {
+      setPhoto();
+    }
+
+    if (ct > tp + part_iv_duration + 350) {
+      removePhoto();
+    }
+  }
+
+  if (ct < tb) {
+    timeoutBall = setTimeout(function () {
+      setBall();
+    }, (tb - ct) * 1000 - 350);
+  } else {// setBall()
+  }
+
+  if (ct < tg) {
+    timeoutGifts = setTimeout(function () {
+      setGifts();
+    }, (tg - ct) * 1000 - 350);
+  } else {// setPhoto()
+  }
+};
+
+var player = videojs("video", {
+  flash: options,
+  html5: options,
+  sources: [{
+    src: "/playlist/" + hash + ".m3u8?resolution=" + resolution,
+    type: "application/x-mpegURL"
+  }],
+  controls: true,
+  control: true,
+  poster: "https://montage-cache.cdnvideo.ru/montage/.previews/preview-5fae91b4ef3db56d66205367.jpg"
+}, function () {
+  var that = this;
+  this.on("ended", function () {
+    checkTimeouts(that);
+  });
+  this.on("play", function () {
+    checkTimeouts(that);
+  });
+  this.on("pause", function () {
+    checkTimeouts(that);
+  });
+  this.on("firstplay", function () {
+    checkTimeouts(that);
+  });
+  this.on("change", function () {
+    checkTimeouts(that);
+  });
+  this.on("loadedmetadata", function () {
+    checkTimeouts(that);
+  });
+  this.on("progress", function () {
+    checkTimeouts(that);
+  });
+  this.on("seeking", function () {
+    checkTimeouts(that);
+  });
+  this.on("seeked", function () {
+    checkTimeouts(that);
+  });
+});
+
+var draw = function draw() {
+  canvas.getContext("2d").drawImage(player, 0, 0);
+}; // player.addEventListener('play', function(){
+//     if(player.paused || player.ended) return;
+//     draw()
+// })
+
+
+var timeoutPhoto,
+    timeoutRemovePhoto,
+    timeoutBall,
+    setBallPause,
+    timeoutGifts,
+    setGiftsPause,
+    tp = part_i_duration + part_ii_duration + part_iii_duration,
+    tb = tp + part_iv_duration + part_v_duration + part_vi_duration + part_vii_duration,
+    tg = tb + part_viii_duration + part_ix_duration + part_x_duration,
+    photoElement = document.createElement("div"),
+    ballsElement = document.createElement("div"),
+    giftsElement = document.createElement("div"),
+    redGiftElement = document.createElement("div"),
+    whiteGiftElement = document.createElement("div"),
+    goldGiftElement = document.createElement("div");
+
+var chooseBall = function chooseBall(e) {
+  clearTimeout(setBallPause);
+  var videoHeight = player.children()[0].offsetHeight,
+      videoWidth = player.children()[0].offsetWidth;
+
+  if (videoHeight > videoWidth * 720 / 1280) {
+    var width = videoWidth,
+        height = width / 1280 * 720,
+        top = (videoHeight - height) / 2,
+        left = 0;
+  } else {
+    var height = videoHeight,
+        width = height / 720 * 1280;
+    top = 0, left = (videoWidth - width) / 2;
+  }
+
+  var color = "g";
+  var margin = (window.innerWidth - width) / 2;
+  var clientX = e.changedTouches ? e.changedTouches[0].clientX : e.clientX;
+  if (margin + width * 0.375 < clientX) color = "r";
+  if (margin + (width - width * 0.375) < clientX) color = "s";
+  var segments = player.tech({
+    IWillNotUseThisInPlugins: true
+  }).hls.playlists.master.playlists[0].segments;
+  var newSegments = [];
+
+  for (i in segments) {
+    // console.log(segments[i].uri.indexOf("part_ix"), i, color);
+    if (segments[i].uri.indexOf("part_ix") > -1) {
+      segments[i].resolvedUri = cdn + "part_ix/" + color + "%20%28" + resolution + "xauto%29.mp4/media_0.ts";
+      segments[i].uri = cdn + "part_ix/" + color + "%20%28" + resolution + "xauto%29.mp4/media_0.ts";
+    }
+
+    newSegments.push(segments[i]);
+  }
+
+  console.log(player, player.tech({
+    IWillNotUseThisInPlugins: true
+  }), player.tech({
+    IWillNotUseThisInPlugins: true
+  }).hls); // player.tech({ IWillNotUseThisInPlugins: true }).hls.resyncLoader();resetEverything
+
+  console.log(player.tech({
+    IWillNotUseThisInPlugins: true
+  }).hls.mainSegmentLoader);
+  console.log(player.tech({
+    IWillNotUseThisInPlugins: true
+  }).hls.mainSegmentLoader());
+  player.trigger("mediachange");
+  player.tech({
+    IWillNotUseThisInPlugins: true
+  }).trigger("mediachange");
+  player.tech({
+    IWillNotUseThisInPlugins: true
+  }).hls.trigger("mediachange"); // player.tech({ IWillNotUseThisInPlugins: true }).hls.updateSegments(segments, newSegments);
+
+  setTimeout(function () {
+    removeBalls(); // player.currentTime(tb + part_viii_duration + part_ix_duration + 200);
+
+    player.play();
+  }, 500);
+};
+
+var removeBalls = function removeBalls() {
+  console.log("removeBalls");
+  ballsElement.remove();
+};
+
+var setBall = function setBall() {
+  console.log("setBall");
+  clearTimeout(setBallPause);
+  var ct = player.currentTime();
+  ballsElement = createEl();
+  ballsElement.style.background = "url('https://montage-cache.cdnvideo.ru/montage/kindern/part_viii/balls.png') no-repeat 0 0 / 100%";
+  document.getElementById("video").appendChild(ballsElement);
+  ballsElement.addEventListener("touchstart", chooseBall);
+  ballsElement.addEventListener("click", chooseBall);
+  setBallPause = setTimeout(function () {
+    player.pause();
+  }, (tb + part_viii_duration - ct) * 1000 - 500);
+};
+
+var chooseGift = function chooseGift(e) {
+  clearTimeout(setGiftsPause);
+  clearTimeout(setBallPause);
+  console.log("chooseGift");
+  e.target.style.opacity = "1";
+
+  if (redImg.style.opacity == "1" && whiteImg.style.opacity == "1" && goldImg.style.opacity == "1") {
+    player.currentTime(tg + part_xi_duration + 0.5);
+    player.play();
+    setTimeout(function () {
+      removeGifts();
+    }, 200);
+  }
+};
+
+var setGifts = function setGifts() {
+  console.log("setGifts");
+  clearTimeout(setGiftsPause);
+  var ct = player.currentTime();
+  giftsElement = createEl();
+  giftsElement.style.background = "url('https://montage-cache.cdnvideo.ru/montage/kindern/part_xi/podarki.png') no-repeat 0 0 / 100%";
+  document.getElementById("video").appendChild(giftsElement);
+  redImg = document.createElement("img");
+  redImg.src = "https://montage-cache.cdnvideo.ru/montage/kindern/part_xi/red.png";
+  redImg.alt = "";
+  redImg.style.position = "absolute";
+  redImg.style.top = "41.015%";
+  redImg.style.left = "47.1%";
+  redImg.style.opacity = "0";
+  redImg.style.height = "42.4%";
+  whiteImg = document.createElement("img");
+  whiteImg.src = "https://montage-cache.cdnvideo.ru/montage/kindern/part_xi/white.png";
+  whiteImg.alt = "";
+  whiteImg.style.position = "absolute";
+  whiteImg.style.top = "64.715%";
+  whiteImg.style.left = "38.1%";
+  whiteImg.style.opacity = "0";
+  whiteImg.style.height = "31.4%";
+  goldImg = document.createElement("img");
+  goldImg.src = "https://montage-cache.cdnvideo.ru/montage/kindern/part_xi/gold.png";
+  goldImg.alt = "";
+  goldImg.style.position = "absolute";
+  goldImg.style.top = "47.3%";
+  goldImg.style.left = "66%";
+  goldImg.style.opacity = "0";
+  goldImg.style.height = "42.4%";
+  giftsElement.appendChild(redImg);
+  giftsElement.appendChild(whiteImg);
+  giftsElement.appendChild(goldImg);
+  redImg.addEventListener("touchstart", chooseGift);
+  redImg.addEventListener("click", chooseGift);
+  whiteImg.addEventListener("touchstart", chooseGift);
+  whiteImg.addEventListener("click", chooseGift);
+  goldImg.addEventListener("touchstart", chooseGift);
+  goldImg.addEventListener("click", chooseGift);
+  setGiftsPause = setTimeout(function () {
+    player.pause();
+  }, (tg + part_xi_duration - ct) * 1000 - 500);
+};
+
+var setPhoto = function setPhoto() {
+  var ct = player.currentTime();
+  photoElement = createEl();
+  photoElement.style.background = "url('https://montage-cache.cdnvideo.ru/montage/kindern/part_iv/photo.png') no-repeat 0 0 / 100%, url('" + photo + "') no-repeat top left 57%/auto 92%";
+  document.getElementById("video").appendChild(photoElement);
+  clearTimeout(timeoutRemovePhoto);
+  timeoutRemovePhoto = setTimeout(function () {
+    removePhoto();
+  }, (tp + part_iv_duration - ct) * 1000 + 350);
+};
+
+var removeGifts = function removeGifts() {
+  console.log("removeGifts");
+  document.getElementById("video").removeChild(giftsElement);
+};
+
+var removePhoto = function removePhoto() {
+  console.log("removePhoto");
+  document.getElementById("video").removeChild(photoElement);
+};
+
+var createEl = function createEl() {
+  player.exitFullscreen();
+  window.scrollTo(0, 1);
+  var videoHeight = player.el().offsetHeight,
+      videoWidth = player.el().offsetWidth;
+
+  if (videoHeight > videoWidth * 720 / 1280) {
+    var width = videoWidth,
+        height = width / 1280 * 720,
+        top = (videoHeight - height) / 2,
+        left = 0;
+  } else {
+    var height = videoHeight,
+        width = height / 720 * 1280;
+    top = 0, left = (videoWidth - width) / 2;
+  }
+
+  el = document.createElement("div");
+  el.style.position = "absolute";
+  el.style.height = height + "px";
+  el.style.width = width + "px";
+  el.style.top = top + "px";
+  el.style.left = left + "px";
+  el.style.zIndex = "100";
+  el.style.backgroundColor = "#000000";
+  return el;
+};
 
 /***/ }),
 
