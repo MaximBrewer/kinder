f6047c29e3e7680c34b341f746f851af58563009
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-14 16:36:45 +0600

    dfdf

--- resources/js/video.js
+++ resources/js/video.js
@@ -144,6 +144,7 @@ var chooseBall = function(e) {
     if (margin + width * 0.375 < clientX) color = "r";
     if (margin + (width - width * 0.375) < clientX) color = "s";
 
+    alert(0)
     var segments = player.tech({ IWillNotUseThisInPlugins: true }).hls.playlists
         .master.playlists[0].segments;
     var start = 0;
