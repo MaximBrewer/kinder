96450fed0c0c3f5313e2f7ba4c63446c7c2c0219
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-16 05:46:14 +0600

    fvf

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -166,11 +166,12 @@ class SiteController extends Controller
         if($request->get('name'))
             $orderName = \App\Models\Name::find($request->get('name'));
 
+            var_dump($orderName->id);
 
         if (!$orderName->chunks640) $this->setChunks($orderName, 'part_ii', 640);
         if (!$orderName->chunks1024) $this->setChunks($orderName, 'part_ii', 1024);
-        if (!$orderName->chunks1920) $this->setChunks($orderName, 'part_ii', 1920);
-        if (!$orderName->chunks1280) $duration = $this->setChunks($orderName, 'part_ii', 1280);
+        if (!$orderName->chunks1920) $this->setChunks($orderName, 'part_ii', 1280);
+        if (!$orderName->chunks1920) $duration = $this->setChunks($orderName, 'part_ii', 1920);
         else $duration = $this->countDuration($orderName->chunks1920);
         $data['part_ii_duration'] = $duration;
 
