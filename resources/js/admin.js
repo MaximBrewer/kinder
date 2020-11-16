a219550c074de46618d1b37ea57d48bcf0b37032
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-16 20:12:13 +0600

    sd

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,20 +1,25 @@
 $(function() {
-    $(".rotate-image").click((e) => {
-        e.stopPropagation()
-        console.log(e.target, this)
-        let res = $.get("/admin/orders/" + $(this).data("id") + "/rotate");
-        if (res.status == 200) {
-            let img = $(el)
-                .closest("tr")
-                .find(
-                    "img[src=https://kinder.gpucloud.ru" +
-                        res.responseText +
-                        "]"
+    const rotators = Object.values(
+        document.getElementsByClassName("rotate-image")
+    );
+    rotators.forEach(rotator => {
+        rotator.addEventListener("click", event => {
+            event.preventDefault();
+            console.log(e.target, this);
+            let res = $.get("/admin/orders/" + $(this).data("id") + "/rotate");
+            if (res.status == 200) {
+                let img = $(el)
+                    .closest("tr")
+                    .find(
+                        "img[src=https://kinder.gpucloud.ru" +
+                            res.responseText +
+                            "]"
+                    );
+                img.attr(
+                    "src",
+                    img.attr("src").split("?")[0] + "?" + Math.random()
                 );
-            img.attr(
-                "src",
-                img.attr("src").split("?")[0] + "?" + Math.random()
-            );
-        }
+            }
+        });
     });
 });
--- resources/js/dist/admin.dev.js
+++ resources/js/dist/admin.dev.js
@@ -3,14 +3,17 @@
 $(function () {
   var _this = this;
 
-  $(".rotate-image").click(function (e) {
-    e.stopPropagation();
-    console.log(e.target, _this);
-    var res = $.get("/admin/orders/" + $(_this).data("id") + "/rotate");
+  var rotators = Object.values(document.getElementsByClassName("rotate-image"));
+  rotators.forEach(function (rotator) {
+    rotator.addEventListener("click", function (event) {
+      event.preventDefault();
+      console.log(e.target, _this);
+      var res = $.get("/admin/orders/" + $(_this).data("id") + "/rotate");
 
-    if (res.status == 200) {
-      var img = $(el).closest("tr").find("img[src=https://kinder.gpucloud.ru" + res.responseText + "]");
-      img.attr("src", img.attr("src").split("?")[0] + "?" + Math.random());
-    }
+      if (res.status == 200) {
+        var img = $(el).closest("tr").find("img[src=https://kinder.gpucloud.ru" + res.responseText + "]");
+        img.attr("src", img.attr("src").split("?")[0] + "?" + Math.random());
+      }
+    });
   });
 });
