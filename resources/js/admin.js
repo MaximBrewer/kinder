1b8f5e147789ec833168885c74d0355cf2ee05f1
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-16 20:05:48 +0600

    sdsd

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -2,10 +2,20 @@ $(function() {
     $(".rotate-image").on("click", e => {
         e.preventDefault();
         let el = e.target;
+        console.log($(el), $(el).data("id"));
         let res = $.get("/admin/orders/" + $(el).data("id") + "/rotate");
         if (res.status == 200) {
-            let img = $(el).closest('tr').find('img[src=https://kinder.gpucloud.ru' + res.responseText + ']');
-            img.attr('src', img.attr("src").split("?")[0] + "?" + Math.random())
+            let img = $(el)
+                .closest("tr")
+                .find(
+                    "img[src=https://kinder.gpucloud.ru" +
+                        res.responseText +
+                        "]"
+                );
+            img.attr(
+                "src",
+                img.attr("src").split("?")[0] + "?" + Math.random()
+            );
         }
     });
 });
--- resources/js/dist/admin.dev.js
+++ resources/js/dist/admin.dev.js
@@ -4,11 +4,12 @@ $(function () {
   $(".rotate-image").on("click", function (e) {
     e.preventDefault();
     var el = e.target;
+    console.log($(el), $(el).data("id"));
     var res = $.get("/admin/orders/" + $(el).data("id") + "/rotate");
 
     if (res.status == 200) {
-      var img = $(el).closest('tr').find('img[src=https://kinder.gpucloud.ru' + res.responseText + ']');
-      img.attr('src', img.attr("src").split("?")[0] + "?" + Math.random());
+      var img = $(el).closest("tr").find("img[src=https://kinder.gpucloud.ru" + res.responseText + "]");
+      img.attr("src", img.attr("src").split("?")[0] + "?" + Math.random());
     }
   });
 });
