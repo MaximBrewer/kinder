e909d3704d0354ad4ef2166bcdcf61f058a19967
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-16 19:22:35 +0600

    sds

--- public/js/admin.js
+++ public/js/admin.js
@@ -95,9 +95,20 @@
 
 var rotators = Object.values(document.getElementsByClassName("rotate-image"));
 rotators.forEach(function (rotator) {
-  rotator.addEventListener('click', function (event) {
+  rotator.addEventListener("click", function (event) {
     event.preventDefault();
     console.log(event.target.dataset.id);
+    var request = new XMLHttpRequest();
+    var url = "/admin/orders/" + event.target.dataset.id + "/rotate";
+    request.responseType = "json";
+    request.open("GET", url, true);
+    request.addEventListener("readystatechange", function () {
+      if (request.readyState === 4 && request.status === 200) {
+        var obj = request.response;
+        var arr = obj.results;
+      }
+    });
+    request.send();
   });
 });
 
--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,8 +1,20 @@
 const rotators = Object.values(document.getElementsByClassName("rotate-image"));
 
 rotators.forEach(rotator => {
-    rotator.addEventListener('click', event => {
+    rotator.addEventListener("click", event => {
         event.preventDefault();
         console.log(event.target.dataset.id);
+
+        const request = new XMLHttpRequest();
+        const url = "/admin/orders/" + event.target.dataset.id + "/rotate";
+        request.responseType = "json";
+        request.open("GET", url, true);
+        request.addEventListener("readystatechange", () => {
+            if (request.readyState === 4 && request.status === 200) {
+                let obj = request.response;
+                let arr = obj.results;
+            }
+        });
+        request.send();
     });
 });
