48a9bceee7c57b0f1007da37e4ff9b4ab7943b9d
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-16 20:04:19 +0600

    sdsd

--- public/js/admin.js
+++ public/js/admin.js
@@ -96,7 +96,13 @@
 $(function () {
   $(".rotate-image").on("click", function (e) {
     e.preventDefault();
-    console.log($.get("/admin/orders/" + $(e.target).data("id") + "/rotate"));
+    var el = $(e.target);
+    var res = $.get("/admin/orders/" + $(el).data("id") + "/rotate");
+
+    if (res.status == 200) {
+      var img = $(el).closest('tr').find('img[src=https://kinder.gpucloud.ru' + res.responseText + ']');
+      img.attr('src', img.attr("src").split("?")[0] + "?" + Math.random());
+    }
   });
 });
 
--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,8 +1,11 @@
 $(function() {
     $(".rotate-image").on("click", e => {
         e.preventDefault();
-        console.log(
-            $.get("/admin/orders/" + $(e.target).data("id") + "/rotate")
-        );
+        let el = $(e.target);
+        let res = $.get("/admin/orders/" + $(el).data("id") + "/rotate");
+        if (res.status == 200) {
+            let img = $(el).closest('tr').find('img[src=https://kinder.gpucloud.ru' + res.responseText + ']');
+            img.attr('src', img.attr("src").split("?")[0] + "?" + Math.random())
+        }
     });
 });
