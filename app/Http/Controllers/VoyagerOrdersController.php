ac74b0204d6d6aee06bcba5e4bc9a547d51f3429
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-16 19:49:13 +0600

    sds

--- app/Http/Controllers/VoyagerOrdersController.php
+++ app/Http/Controllers/VoyagerOrdersController.php
@@ -62,7 +62,7 @@ class VoyagerOrdersController extends \TCG\Voyager\Http\Controllers\Controller
     public function rotate(Request $request, $id)
     {
         $order = Order::findOrFail($id);
-        return(Image::make(storage_path("app/public/" . $order->photo))->rotate(-90, 0)->save(storage_path("app/public/" . $order->photo)));
+        Image::make(storage_path("app/public/" . $order->photo))->rotate(-90, 0)->save(storage_path("app/public/" . $order->photo));
 
         // $exif = exif_read_data($request->file('image_file'));
         // if (!empty($exif['Orientation'])) {
@@ -78,7 +78,7 @@ class VoyagerOrdersController extends \TCG\Voyager\Http\Controllers\Controller
         //             break;
         //     }
         // }
-        return $image;
+        return storage_path("app/public/" . $order->photo);
     }
 
     public function index(Request $request)
