89ddc85b8bb248498edbf7be30130bb3ddd1965f
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-17 17:06:30 +0600

    sdsd

--- resources/views/chunks/part_vii/1280.blade.php
+++ resources/views/chunks/part_vii/1280.blade.php
@@ -3,5 +3,5 @@
 {{ $cdn }}part_vii/pf7%20%281280xauto%29.mp4/media_0.ts
 #EXTINF:12,
 {{ $cdn }}part_vii/pf7%20%281280xauto%29.mp4/media_1.ts
-#EXTINF:0.933,
+#EXTINF:1.1,
 {{ $cdn }}part_vii/pf7%20%281280xauto%29.mp4/media_2.ts
