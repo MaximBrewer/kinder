53d352a77db546167462559eb3ffa808052823e5
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-12-09 01:51:52 +0600

    sds

--- resources/views/chunks/part_i/girl/1024.blade.php
+++ resources/views/chunks/part_i/girl/1024.blade.php
@@ -1,12 +1,10 @@
-#EXTINF:12,
-{{ $cdn }}part_i/pf1girl%20%281024xauto%29.mp4/media_0.ts
-#EXTINF:12,
-{{ $cdn }}part_i/pf1girl%20%281024xauto%29.mp4/media_1.ts
-#EXTINF:12,
-{{ $cdn }}part_i/pf1girl%20%281024xauto%29.mp4/media_2.ts
-#EXTINF:12,
-{{ $cdn }}part_i/pf1girl%20%281024xauto%29.mp4/media_3.ts
-#EXTINF:12,
-{{ $cdn }}part_i/pf1girl%20%281024xauto%29.mp4/media_4.ts
-#EXTINF:11.517,
-{{ $cdn }}part_i/pf1girl%20%281024xauto%29.mp4/media_5.ts
+#EXTINF:16,
+{{ $cdn }}part_i/p1girl-1024.mp4/media_0.ts
+#EXTINF:10.04,
+{{ $cdn }}part_i/p1girl-1024.mp4/media_1.ts
+#EXTINF:12.88,
+{{ $cdn }}part_i/p1girl-1024.mp4/media_2.ts
+#EXTINF:15.72,
+{{ $cdn }}part_i/p1girl-1024.mp4/media_3.ts
+#EXTINF:16.955,
+{{ $cdn }}part_i/p1girl-1024.mp4/media_4.ts
