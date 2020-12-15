01ef1423852876430ee9b5caee319fa293c95ee8
MaximBrewer <pimax1978@icloud.com>
2020-12-16 01:12:55 +0600

    sd

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::post('/patch', '\App\Http\Controllers\SiteController@patch');
 Route::get('/test-mail', '\App\Http\Controllers\SiteController@testMail');
 Route::get('/video/{hash}', '\App\Http\Controllers\SiteController@video');
 Route::get('/playlist/{hash}.m3u8', '\App\Http\Controllers\SiteController@playlist');
+Route::get('/uploadlist/{hash}.m3u8', '\App\Http\Controllers\SiteController@uploadlist');
 Route::get('/playlist-color/{hash}.m3u8', '\App\Http\Controllers\SiteController@playlistColor');
 
 Route::group(['prefix' => 'admin'], function () {
