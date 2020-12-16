6ee8516c9ac81428326c6d6e08dc55fa0a4ff8f9
MaximBrewer <pimax1978@icloud.com>
2020-12-16 16:22:52 +0600

    ывы

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -41,7 +41,7 @@
 	<script src="//s.platformcraft.ru/video/videojs/video5.19.2.min.js"></script>
 	<script src="//s.platformcraft.ru/video/videojs/plugins/hls/videojs-contrib-hls-5.9.min.js"></script>
     <script src="//s.platformcraft.ru/video/videojs/plugins/videojs.framebyframe.min.js"></script>
-    <script src="/js/video.js?v=7.03"></script>
+    <script src="/js/video.js?v=8.03"></script>
     <style>
         .video-js:after {
             content: none;
