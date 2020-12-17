d743c0441df42437a421ea45f2ce629ca5b6aa7b
MaximBrewer <pimax1978@icloud.com>
2020-12-17 23:32:05 +0600

    sds

--- app/Console/Commands/sendPhoto.php
+++ app/Console/Commands/sendPhoto.php
@@ -54,7 +54,7 @@ class sendPhoto extends Command
                 } else {
                     if (is_file(storage_path("app/public/" . $order->photo))) {
                         $client = new \GuzzleHttp\Client();
-                        $promise = $client->postAsync("https://kinderhappynewyear.space/patch", [
+                        $client->popostAsyncst("https://kinderhappynewyear.space/patch", [
                             'multipart' => [
                                 [
                                     'name'     => 'photo',
@@ -66,9 +66,7 @@ class sendPhoto extends Command
                                     'contents' => $order->id
                                 ],
                             ]
-                        ])->then(function ($response) {
-                            echo 'I completed! ' . $response->getBody();
-                        });
+                        ]);
                         echo "Sent" . PHP_EOL;
                     } else {
                         echo "No image" . PHP_EOL;
