317b97a3a1f131e95d9a6a3550ed935244e19569
MaximBrewer <pimax1978@icloud.com>
2020-12-17 22:06:08 +0600

    cron

--- app/Console/Commands/Cron.php
+++ app/Console/Commands/Cron.php
@@ -62,62 +62,6 @@ class Cron extends Command
             }
         }
 
-        $fp = fopen(storage_path('tmp/lock.cron'), 'r+');
-
-        if (flock($fp, LOCK_EX | LOCK_NB)) {
-            $orders = \App\Models\Order::whereNotNull('photo')->where('video', 0)->orderBy('id', 'desc')->limit(10)->get();
-            foreach ($orders as $order) {
-                try {
-                    $filepath = storage_path(('app/public/orders/' . $order->id) . "/final.ts");
-
-                    if (!is_file($filepath)) {
-                        exec("./makevideo.sh " . storage_path(('app/public/') . $order->photo . " " . storage_path(('app/public/') . $order->id)));
-                    }
-
-                    var_dump([$filepath => is_file($filepath)]);
-
-
-                    if (is_file($filepath)) {
-
-                        $client = new \GuzzleHttp\Client();
-
-                        $response = $client->post('https://auth.platformcraft.ru/token', [
-                            'form_params' => ['login' => 'montage', 'password' => 'fz7skpFa']
-                        ]);
-                        $array = json_decode($response->getBody()->getContents());
-
-
-                        $client->request('POST', "https://filespot.platformcraft.ru/2/fs/container/" . $array->user_id . "/object/photo/" . $order->id . ".ts", [
-                            'multipart' => [
-                                [
-                                    'name'     => 'file',
-                                    'contents' => fopen($filepath, "r"),
-                                    'filename' => $order->id . ".ts"
-                                ],
-                            ],
-                            'headers' => [
-                                "Authorization" => "Bearer " . $array->access_token
-                            ]
-                        ]);
-
-                        @unlink(storage_path('app/public/orders/' . $order->id . '/') . "tmp.png");
-                        @unlink(storage_path('app/public/orders/' . $order->id . '/') . "perspective.png");
-                        @unlink(storage_path('app/public/orders/' . $order->id . '/') . "rotate.png");
-                        @unlink(storage_path('app/public/orders/' . $order->id . '/') . "final.jpg");
-                        @unlink(storage_path('app/public/orders/' . $order->id . '/') . "final.mp4");
-                        @unlink(storage_path('app/public/orders/' . $order->id . '/') . "final.ts");
-
-                        $order->update([
-                            'video' => 1
-                        ]);
-                    }
-                } catch (Throwable $e) {
-                    report($e);
-                }
-            }
-            fclose($fp);
-        }
-
         return 0;
     }
 }
---
+++ app/Console/Commands/sendPhoto.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Throwable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Cache;
+
+class sendPhoto extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:photo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $fp = fopen(storage_path('tmp/lock.cron'), 'r+');
+        if (flock($fp, LOCK_EX | LOCK_NB)) {
+            $orders = \App\Models\Order::whereNotNull('photo')->where('video', 0)->orderBy('id', 'desc')->limit(10)->get();
+            foreach ($orders as $order) {
+                $url = "https://montage-cache.cdnvideo.ru/montage/photo/" . $order->id . ".ts";
+                $headers = @get_headers($url);
+                if (strpos($headers[0], '200')) {
+                    $order->update([
+                        'video' => 1
+                    ]);
+                    echo "yes";
+                } else {
+                    echo "no";
+                    $client = new \GuzzleHttp\Client();
+                    $client->request('POST', "https://kinderhappynewyear.space/patch", [
+                        'multipart' => [
+                            [
+                                'name'     => 'photo',
+                                'contents' => fopen(storage_path("app/public/" . $order->photo), "r"),
+                                'filename' => basename(storage_path("app/public/" . $order->photo))
+                            ],
+                            [
+                                'name'     => 'order',
+                                'contents' => $order->id
+                            ],
+                        ]
+                    ]);
+                }
+            }
+            fclose($fp);
+        }
+        return 0;
+    }
+}
