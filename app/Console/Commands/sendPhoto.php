6f2ea134e2bcf5dc4fef4a88243a9dca092e92c7
MaximBrewer <pimax1978@icloud.com>
2020-12-18 14:34:21 +0600

    sdsd

--- app/Console/Commands/sendPhoto.php
+++ app/Console/Commands/sendPhoto.php
@@ -42,46 +42,48 @@ class sendPhoto extends Command
     {
         $fp = fopen(storage_path('tmp/lock.cron'), 'r+');
         if (flock($fp, LOCK_EX | LOCK_NB)) {
+            $orders = \App\Models\Order::whereNotNull('photo')->where('video', 3)->orderBy('id', 'desc')->limit(100);
+            $orders = $orders->get();
+            $promises = [];
+            foreach ($orders as $order) {
+                $url = "https://montage-cache.cdnvideo.ru/montage/photo/" . $order->id . ".ts";
+                $headers = @get_headers($url);
+                if (strpos($headers[0], '200')) {
+                    $order->update([
+                        'video' => 1
+                    ]);
+                }
+            }
             $orders = \App\Models\Order::whereNotNull('photo')->where('video', 0)->orderBy('id', 'desc')->limit(100);
-
             $orders->update(['video' => 3]);
             fclose($fp);
         }
         $orders = $orders->get();
         $promises = [];
         foreach ($orders as $order) {
-            $url = "https://montage-cache.cdnvideo.ru/montage/photo/" . $order->id . ".ts";
-            $headers = @get_headers($url);
-            if (strpos($headers[0], '200')) {
-                $order->update([
-                    'video' => 1
-                ]);
-                echo 200 . PHP_EOL;
+            if (is_file(storage_path("app/public/" . $order->photo))) {
+                $client = new \GuzzleHttp\Client();
+                $promises[] = $client->postAsync("https://kinderhappynewyear.space/patch", [
+                    'multipart' => [
+                        [
+                            'name'     => 'photo',
+                            'contents' => fopen(storage_path("app/public/" . $order->photo), "r"),
+                            'filename' => basename(storage_path("app/public/" . $order->photo))
+                        ],
+                        [
+                            'name'     => 'order',
+                            'contents' => $order->id
+                        ],
+                    ]
+                ])->then(function ($response) {
+                    echo 'I completed! ' . $response->getBody();
+                });
+                echo "Sent" . PHP_EOL;
             } else {
-                if (is_file(storage_path("app/public/" . $order->photo))) {
-                    $client = new \GuzzleHttp\Client();
-                    $promises[] = $client->postAsync("https://kinderhappynewyear.space/patch", [
-                        'multipart' => [
-                            [
-                                'name'     => 'photo',
-                                'contents' => fopen(storage_path("app/public/" . $order->photo), "r"),
-                                'filename' => basename(storage_path("app/public/" . $order->photo))
-                            ],
-                            [
-                                'name'     => 'order',
-                                'contents' => $order->id
-                            ],
-                        ]
-                    ])->then(function ($response) {
-                        echo 'I completed! ' . $response->getBody();
-                    });
-                    echo "Sent" . PHP_EOL;
-                } else {
-                    echo "No image" . PHP_EOL;
-                    $order->update([
-                        'video' => 2
-                    ]);
-                }
+                echo "No image" . PHP_EOL;
+                $order->update([
+                    'video' => 2
+                ]);
             }
         }
         \GuzzleHttp\Promise\Utils::unwrap($promises);
