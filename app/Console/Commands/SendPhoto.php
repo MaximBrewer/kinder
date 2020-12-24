31ce850fabfb63bc6c424e17226baf663fcac369
MaximBrewer <pimax1978@icloud.com>
2020-12-24 06:59:51 +0600

    sdsd

--- app/Console/Commands/SendPhoto.php
+++ app/Console/Commands/SendPhoto.php
@@ -62,38 +62,38 @@ class SendPhoto extends Command
             $orders->update(['video' => 3]);
             fclose($fp);
         }
-        if (isset($orders)) {
-            $orders = $orders->get();
-            $promises = [];
-            foreach ($orders as $order) {
-                $pathf = storage_path("app/public/orders/" . $order->id . "/final.jpg");
-                if (is_file($pathf)) {
-                    $client = new \GuzzleHttp\Client();
-                    $promises[] = $client->postAsync("https://kinderhappynewyear.space/patch", [
-                        'multipart' => [
-                            [
-                                'name'     => 'photo',
-                                'contents' => fopen($pathf, "r"),
-                                'filename' => basename($pathf)
-                            ],
-                            [
-                                'name'     => 'order',
-                                'contents' => $order->id
-                            ],
-                        ]
-                    ])->then(function ($response) {
-                        echo 'I completed! ' . $response->getBody();
-                    });
-                    echo "Sent" . PHP_EOL;
-                } else {
-                    echo "No image" . PHP_EOL;
-                    $order->update([
-                        'video' => 2
-                    ]);
-                }
-            }
-            \GuzzleHttp\Promise\Utils::unwrap($promises);
-        }
+        // if (isset($orders)) {
+        //     $orders = $orders->get();
+        //     $promises = [];
+        //     foreach ($orders as $order) {
+        //         $pathf = storage_path("app/public/orders/" . $order->id . "/final.jpg");
+        //         if (is_file($pathf)) {
+        //             $client = new \GuzzleHttp\Client();
+        //             $promises[] = $client->postAsync("https://kinderhappynewyear.space/patch", [
+        //                 'multipart' => [
+        //                     [
+        //                         'name'     => 'photo',
+        //                         'contents' => fopen($pathf, "r"),
+        //                         'filename' => basename($pathf)
+        //                     ],
+        //                     [
+        //                         'name'     => 'order',
+        //                         'contents' => $order->id
+        //                     ],
+        //                 ]
+        //             ])->then(function ($response) {
+        //                 echo 'I completed! ' . $response->getBody();
+        //             });
+        //             echo "Sent" . PHP_EOL;
+        //         } else {
+        //             echo "No image" . PHP_EOL;
+        //             $order->update([
+        //                 'video' => 2
+        //             ]);
+        //         }
+        //     }
+        //     \GuzzleHttp\Promise\Utils::unwrap($promises);
+        // }
         return 0;
     }
 }
