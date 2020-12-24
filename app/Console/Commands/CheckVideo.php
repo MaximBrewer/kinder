338e74bd529c3d9fd4c794f7b41d08be6d27368a
MaximBrewer <pimax1978@icloud.com>
2020-12-24 07:01:07 +0600

    dvfd

---
+++ app/Console/Commands/CheckVideo.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CheckVideo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:video';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $fp = fopen(storage_path('tmp/lock.cron'), 'r+');
+        if (flock($fp, LOCK_EX | LOCK_NB)) {
+            $orders = \App\Models\Order::where('video', 3)->orderBy('id', 'desc')->limit(50);
+            $orders = $orders->get();
+            $promises = [];
+            foreach ($orders as $order) {
+                $url = "https://montage-cache.cdnvideo.ru/montage/photo/" . $order->id . ".ts";
+                $headers = @get_headers($url);
+                if (strpos($headers[0], '200')) {
+                    $order->update([
+                        'video' => 1
+                    ]);
+                } else {
+                    $order->update([
+                        'video' => 0
+                    ]);
+                }
+            }
+            $orders = \App\Models\Order::whereNotNull('photo')->where('video', 0)->where('pic', 1)->orderBy('id', 'desc')->limit(50);
+            $orders->update(['video' => 3]);
+            fclose($fp);
+        }
+        return 0;
+    }
+}
