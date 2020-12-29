65b693620b82a9113945d222a6ac41f3a5ab0e4f
MaximBrewer <pimax1978@icloud.com>
2020-12-29 12:01:13 +0600

    sds

---
+++ app/Console/Commands/CheckPhoto.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CheckPhoto extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:photo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $orders = \App\Models\Order::where('video', 3)->orderBy('id', 'desc')->limit(1000);
+        $orders = $orders->get();
+        foreach ($orders as $order) {
+            $url = "/var/www/html/kinder.gpucloud.ru/storage/app/public/orders/334784/" . $order->id . ".ts";
+            echo filesize($url) . PHP_EOL;
+        }
+        return 0;
+    }
+}
