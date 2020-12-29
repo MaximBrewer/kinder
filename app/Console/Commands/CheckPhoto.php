5df045576cee0d6f7fadd29eae05ac6a48748552
MaximBrewer <pimax1978@icloud.com>
2020-12-29 12:03:18 +0600

    ывы

--- app/Console/Commands/CheckPhoto.php
+++ app/Console/Commands/CheckPhoto.php
@@ -37,7 +37,7 @@ class CheckPhoto extends Command
      */
     public function handle()
     {
-        $orders = \App\Models\Order::where('video', 3)->orderBy('id', 'desc')->limit(1000);
+        $orders = \App\Models\Order::orderBy('id', 'desc')->limit(1000);
         $orders = $orders->get();
         foreach ($orders as $order) {
             $url = "/var/www/html/kinder.gpucloud.ru/storage/app/public/orders/334784/" . $order->id . ".ts";
