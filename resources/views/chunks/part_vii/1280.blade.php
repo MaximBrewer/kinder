f303fdf5aaf0a20ba1378225dfef9ed0d8ba949c
MaximBrewer <pimax1978@icloud.com>
2020-12-30 16:30:18 +0600

    ывы

--- resources/views/chunks/part_vii/1280.blade.php
+++ resources/views/chunks/part_vii/1280.blade.php
@@ -1,7 +1,7 @@
 #EXTINF:10.8,
 #EXT-X-DISCONTINUITY
-{{ $cdn }}part_vii/pt7%20%281280xauto%29.mp4/media_0.ts
+{{ $cdn }}part_vii/pc7%20%281280xauto%29.mp4/media_0.ts
 #EXTINF:10.8,
-{{ $cdn }}part_vii/pt7%20%281280xauto%29.mp4/media_1.ts
+{{ $cdn }}part_vii/pc7%20%281280xauto%29.mp4/media_1.ts
 #EXTINF:3.32,
-{{ $cdn }}part_vii/pt7%20%281280xauto%29.mp4/media_2.ts
+{{ $cdn }}part_vii/pc7%20%281280xauto%29.mp4/media_2.ts
--- resources/views/chunks/part_vii/1920.blade.php
+++ resources/views/chunks/part_vii/1920.blade.php
@@ -1,7 +1,7 @@
 #EXTINF:10,
 #EXT-X-DISCONTINUITY
-{{ $cdn }}part_vii/pf7%20%281920x1080%29.mp4/media_0.ts
+{{ $cdn }}part_vii/pc7%20%281920x1080%29.mp4/media_0.ts
 #EXTINF:10,
-{{ $cdn }}part_vii/pf7%20%281920x1080%29.mp4/media_1.ts
+{{ $cdn }}part_vii/pc7%20%281920x1080%29.mp4/media_1.ts
 #EXTINF:4.919,
-{{ $cdn }}part_vii/pf7%20%281920x1080%29.mp4/media_2.ts
+{{ $cdn }}part_vii/pc7%20%281920x1080%29.mp4/media_2.ts
