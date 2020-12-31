7c37dfb808f760ab9dc5a8a3d58067813e3752a3
MaximBrewer <pimax1978@icloud.com>
2020-12-31 17:15:57 +0600

    sds

--- app/Console/Commands/Cron.php
+++ app/Console/Commands/Cron.php
@@ -46,6 +46,7 @@ class Cron extends Command
         $orders->update(['sent' => '4']);
         $orders = $orders->get();
         foreach ($orders as $order) {
+            $orders->update(['sent' => '5']);
             try {
                 $unsubscribe = "https://kinder.gpucloud.ru/unsubscribe?email=" . $order->email . "&email_hash=" . $order->email_hash;
                 Mail::to($order->email)->send(new \App\Mail\Frame3($unsubscribe, $order->hash));
